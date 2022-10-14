71182371bb13c1a97e0048428fb031f253d0bef9
Ryan Smith <r@32k.io>
2022-10-13 17:20:10 -0700

    use 55 instead of abrev. 5 or 56

--- rlp/rlp.go
+++ rlp/rlp.go
@@ -3,11 +3,11 @@ package rlp
 import "encoding/binary"
 
 const (
-	str1L, str1H   byte = 000, 127
-	str5L, str5H   byte = 128, 183
-	strNL, strNH   byte = 184, 191
-	list5L, list5H byte = 192, 247
-	listNL, listNH byte = 248, 255
+	str1L, str1H     byte = 000, 127
+	str55L, str55H   byte = 128, 183
+	strNL, strNH     byte = 184, 191
+	list55L, list55H byte = 192, 247
+	listNL, listNH   byte = 248, 255
 )
 
 type Item struct {
@@ -20,14 +20,14 @@ func Encode(input *Item) []byte {
 		switch n := len(input.D); {
 		case n == 1 && input.D[0] <= str1H:
 			return input.D
-		case n < 56:
+		case n <= 55:
 			return append(
-				[]byte{str5L + byte(n)},
+				[]byte{str55L + byte(n)},
 				input.D...,
 			)
 		default:
 			return append(
-				encodeLength(str5H, len(input.D)),
+				encodeLength(str55H, len(input.D)),
 				input.D...,
 			)
 		}
@@ -37,14 +37,14 @@ func Encode(input *Item) []byte {
 	for i := range input.L {
 		out = append(out, Encode(input.L[i])...)
 	}
-	if len(out) < 56 {
+	if len(out) <= 55 {
 		return append(
-			[]byte{list5L + byte(len(out))},
+			[]byte{list55L + byte(len(out))},
 			out...,
 		)
 	}
 	return append(
-		encodeLength(list5H, len(out)),
+		encodeLength(list55H, len(out)),
 		out...,
 	)
 }
@@ -70,11 +70,11 @@ func Decode(input []byte) *Item {
 	switch {
 	case input[0] <= str1H:
 		return &Item{D: []byte{input[0]}}
-	case input[0] <= str5H:
+	case input[0] <= str55H:
 		n := input[0] - str1H
 		return &Item{D: input[1:n]}
 	case input[0] <= strNH:
-		headerSize := 1 + input[0] - str5H
+		headerSize := 1 + input[0] - str55H
 		return &Item{D: input[headerSize:]}
 	default:
 		// The first byte indicates a list
@@ -85,7 +85,7 @@ func Decode(input []byte) *Item {
 		// We advance the cursor past the length.
 		i := 1
 		if input[0] >= listNL {
-			i += int(input[0] - list5H)
+			i += int(input[0] - list55H)
 		}
 
 		item := &Item{L: []*Item{}}
@@ -95,20 +95,20 @@ func Decode(input []byte) *Item {
 			case input[i] <= str1H:
 				// 1 byte string
 				n = 1
-			case input[i] <= str5H:
-				// < 55 byte string
+			case input[i] <= str55H:
+				// <= 55 byte string
 				n += 1 //header byte
-				n += int(input[i] - str5L)
+				n += int(input[i] - str55L)
 			case input[i] <= strNH:
 				// > 55 byte string
 				n += 1 //header byte
-				n += decodeLength(str5H, input[i:])
-			case input[i] <= list5H:
+				n += decodeLength(str55H, input[i:])
+			case input[i] <= list55H:
 				n += 1 // header byte
-				n += int(input[i] - list5L)
+				n += int(input[i] - list55L)
 			default:
 				n += 1 //header byte
-				n += decodeLength(list5H, input[i:])
+				n += decodeLength(list55H, input[i:])
 			}
 
 			item.L = append(item.L, Decode(input[i:i+n]))
