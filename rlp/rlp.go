6588ecf336e14b549bfd1c1d1f1941bbfd1b83c7
Ryan Smith <r@32k.io>
2022-10-15 17:38:19 -0700

    rlp: new package for encoding / decoding rlp data

---
+++ rlp/rlp.go
@@ -0,0 +1,189 @@
+// This package implements a basic encoder/decoder for
+// Ethereum's Recursive-Length Prefix (RLP) Serialization.
+// For a detailed description of RLP, see Ethereum's  page:
+// https://ethereum.org/en/developers/docs/data-structures-and-encoding/rlp/
+package rlp
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+const (
+	str1L, str1H     byte = 000, 127
+	str55L, str55H   byte = 128, 183
+	strNL, strNH     byte = 184, 191
+	list55L, list55H byte = 192, 247
+	listNL, listNH   byte = 248, 255
+)
+
+// Instead of using standard data types and reflection
+// this package chooses to encode Items.
+// Set D or L but not both.
+// L is a list of *Item so that arbitrarily nested lists
+// can be encoded.
+// D is the data payload for the item.
+type Item struct {
+	D []byte
+	L []*Item
+}
+
+var (
+	ErrTooManyArgs = errors.New("must set D xor L")
+	ErrTooFewArgs  = errors.New("input Item is nil")
+)
+
+func Encode(input *Item) ([]byte, error) {
+	if input == nil {
+		return nil, ErrTooFewArgs
+	}
+	if input.D != nil && input.L != nil {
+		return nil, ErrTooManyArgs
+	}
+	if input.D != nil {
+		switch n := len(input.D); {
+		case n == 1 && input.D[0] <= str1H:
+			return input.D, nil
+		case n <= 55:
+			return append(
+				[]byte{str55L + byte(n)},
+				input.D...,
+			), nil
+		default:
+			return append(
+				encodeLength(str55H, len(input.D)),
+				input.D...,
+			), nil
+		}
+	}
+
+	var out []byte
+	for i := range input.L {
+		b, err := Encode(input.L[i])
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, b...)
+	}
+	if len(out) <= 55 {
+		return append(
+			[]byte{list55L + byte(len(out))},
+			out...,
+		), nil
+	}
+	return append(
+		encodeLength(list55H, len(out)),
+		out...,
+	), nil
+}
+
+func encodeLength(t byte, l int) []byte {
+	// header must be <= 8 bytes
+	buf := make([]byte, 8)
+	// bytes needed to encode length
+	n := binary.PutUvarint(buf, uint64(l))
+	return append(
+		[]byte{byte(uint8(t) + uint8(n))},
+		buf[:n]...,
+	)
+}
+
+// Returns  two values representing the length of the
+// header and payload respectively.
+func decodeLength(t byte, input []byte) (int, int) {
+	n := input[0] - t
+	length, _ := binary.Uvarint(input[1 : n+1])
+	return int(n + 1), int(length)
+}
+
+var (
+	ErrNoBytes      = errors.New("input has no bytes")
+	ErrTooManyBytes = errors.New("input has more bytes than specified by outermost header")
+	ErrTooFewBytes  = errors.New("input has fewer bytes than specified by header")
+)
+
+func Decode(input []byte) (*Item, error) {
+	if len(input) == 0 {
+		return nil, ErrNoBytes
+	}
+	switch {
+	case input[0] <= str1H:
+		if len(input) > 1 {
+			return nil, ErrTooManyBytes
+		}
+		return &Item{D: []byte{input[0]}}, nil
+	case input[0] <= str55H:
+		i, n := 1, int(input[0]-str55L)
+		switch {
+		case len(input) < i+n:
+			return nil, ErrTooFewBytes
+		case len(input) > i+n:
+			return nil, ErrTooManyBytes
+		}
+		return &Item{D: input[i : i+n]}, nil
+	case input[0] <= strNH:
+		i, n := decodeLength(str55H, input)
+		switch {
+		case len(input) < i+n:
+			return nil, ErrTooFewBytes
+		case len(input) > i+n:
+			return nil, ErrTooManyBytes
+		}
+		return &Item{D: input[i : i+n]}, nil
+	default:
+		// The first byte indicates a list
+		// and if the first byte is >= 248 (listNL)
+		// then the list has a length > 55 and
+		// therefore the next (input[0]-247 (list55H))
+		// bytes will describe the length of the list.
+		// We advance the cursor i past the length description.
+		// We also compute the size of the list and check the input
+		// to ensure the size matches the header's length description.
+		var i, listSize int
+		switch {
+		case input[0] <= list55H:
+			i, listSize = 1, int(input[0]-list55L)
+		case input[0] <= listNH:
+			i, listSize = decodeLength(list55H, input)
+		}
+
+		switch {
+		case len(input[i:]) > listSize:
+			return nil, ErrTooManyBytes
+		case len(input[i:]) < listSize:
+			return nil, ErrTooFewBytes
+		}
+
+		item := &Item{L: []*Item{}}
+		for i < len(input) {
+			var headerSize, payloadSize int
+			switch {
+			case input[i] <= str1H:
+				headerSize = 0
+				payloadSize = 1
+			case input[i] <= str55H:
+				headerSize = 1
+				payloadSize = int(input[i] - str55L)
+			case input[i] <= strNH:
+				headerSize, payloadSize = decodeLength(str55H, input[i:])
+			case input[i] <= list55H:
+				headerSize = 1
+				payloadSize = int(input[i] - list55L)
+			default:
+				headerSize, payloadSize = decodeLength(list55H, input[i:])
+			}
+
+			if int(i+headerSize+payloadSize) > len(input) {
+				return nil, ErrTooFewBytes
+			}
+
+			d, err := Decode(input[i : i+headerSize+payloadSize])
+			if err != nil {
+				return nil, err
+			}
+			item.L = append(item.L, d)
+			i += headerSize + payloadSize
+		}
+		return item, nil
+	}
+}
---
+++ rlp/rlp_test.go
@@ -0,0 +1,351 @@
+package rlp
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func FuzzEncode(f *testing.F) {
+	var (
+		numItems uint64 = 10
+		payload         = []byte("hello")
+	)
+	f.Add(numItems, payload)
+	f.Fuzz(func(t *testing.T, n uint64, d []byte) {
+		item := &Item{L: []*Item{}}
+		for i := 0; i < int(n); i++ {
+			item.L = append(item.L, &Item{D: d})
+		}
+		b, err := Encode(item)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got, err := Decode(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(item, got) {
+			t.Errorf("want:\n%v\ngot:\n%v\n", item, got)
+		}
+	})
+}
+
+func BenchmarkEncode(b *testing.B) {
+	payload := []byte("hello world")
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		_, err := Encode(&Item{D: payload})
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func intTo2b(i uint16) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, i)
+	return b
+}
+
+func randBytes(n int) []byte {
+	res := make([]byte, n)
+	rand.Read(res)
+	return res
+}
+
+func TestDecode_Errors(t *testing.T) {
+	cases := []struct {
+		desc  string
+		input []byte
+		err   error
+	}{
+		{
+			"short string no error",
+			[]byte{byte(1)},
+			nil,
+		},
+		{
+			"long string. too many bytes",
+			append(
+				[]byte{
+					byte(str55H + 1),
+					byte(56),
+				},
+				randBytes(57)...,
+			),
+			ErrTooManyBytes,
+		},
+		{
+			"long string. too few bytes",
+			append(
+				[]byte{
+					byte(str55H + 1),
+					byte(56),
+				},
+				randBytes(55)...,
+			),
+			ErrTooFewBytes,
+		},
+	}
+	for _, tc := range cases {
+		_, err := Decode(tc.input)
+		if tc.err == nil {
+			if err != nil {
+				t.Errorf("expected nil error got: %v", err)
+			}
+		} else {
+			if !errors.Is(tc.err, err) {
+				t.Errorf("expected %v got %v", tc.err, err)
+			}
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+	cases := []struct {
+		desc string
+		item *Item
+	}{
+		{
+			"short string",
+			&Item{D: []byte("a")},
+		},
+		{
+			"long string",
+			&Item{D: []byte("Lorem ipsum dolor sit amet, consectetur adipisicing elit")},
+		},
+		{
+			"empty list",
+			&Item{L: []*Item{}},
+		},
+		{
+			"list of short strings",
+			&Item{
+				L: []*Item{
+					&Item{D: []byte("a")},
+					&Item{D: []byte("b")},
+				},
+			},
+		},
+		{
+			"list of long strings",
+			&Item{
+				L: []*Item{
+					&Item{D: []byte("Lorem ipsum dolor sit amet, consectetur adipisicing elit")},
+					&Item{D: []byte("Porem ipsum dolor sit amet, consectetur adipisicing elit")},
+				},
+			},
+		},
+		{
+			"the set theoretical representation of three",
+			&Item{
+				L: []*Item{
+					&Item{L: []*Item{}},
+					&Item{L: []*Item{
+						&Item{L: []*Item{}},
+					}},
+					&Item{L: []*Item{
+						&Item{L: []*Item{}},
+						&Item{L: []*Item{
+							&Item{L: []*Item{}},
+						}},
+					}},
+				},
+			},
+		},
+	}
+	for _, tc := range cases {
+		b, err := Encode(tc.item)
+		if err != nil {
+			t.Error(err)
+		}
+		got, err := Decode(b)
+		if err != nil {
+			t.Errorf("error %s: %s", tc.desc, err)
+		}
+		if !reflect.DeepEqual(tc.item, got) {
+			t.Errorf("%s\nwant:\n%# v\ngot:\n%# v\n", tc.desc, tc.item, got)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	cases := []struct {
+		desc string
+		item *Item
+		want []byte
+		err  error
+	}{
+		{
+			"missing item",
+			nil,
+			[]byte{},
+			ErrTooFewArgs,
+		},
+		{
+			"setting L & D",
+			&Item{D: []byte{}, L: []*Item{}},
+			[]byte{},
+			ErrTooManyArgs,
+		},
+		{
+			"zero byte",
+			&Item{D: []byte{byte(0)}},
+			[]byte{0x00},
+			nil,
+		},
+		{
+			"int 0",
+			&Item{D: []byte{}},
+			[]byte{0x80},
+			nil,
+		},
+		{
+			"int 1024",
+			&Item{D: intTo2b(1024)},
+			[]byte{0x82, 0x04, 0x00},
+			nil,
+		},
+		{
+			"empty string",
+			&Item{D: []byte("")},
+			[]byte{0x80},
+			nil,
+		},
+		{
+			"non-empty string",
+			&Item{D: []byte("Lorem ipsum dolor sit amet, consectetur adipisicing elit")},
+			[]byte{
+				0xB8,
+				0x38,
+				0x4C,
+				0x6F,
+				0x72,
+				0x65,
+				0x6D,
+				0x20,
+				0x69,
+				0x70,
+				0x73,
+				0x75,
+				0x6D,
+				0x20,
+				0x64,
+				0x6F,
+				0x6C,
+				0x6F,
+				0x72,
+				0x20,
+				0x73,
+				0x69,
+				0x74,
+				0x20,
+				0x61,
+				0x6D,
+				0x65,
+				0x74,
+				0x2C,
+				0x20,
+				0x63,
+				0x6F,
+				0x6E,
+				0x73,
+				0x65,
+				0x63,
+				0x74,
+				0x65,
+				0x74,
+				0x75,
+				0x72,
+				0x20,
+				0x61,
+				0x64,
+				0x69,
+				0x70,
+				0x69,
+				0x73,
+				0x69,
+				0x63,
+				0x69,
+				0x6E,
+				0x67,
+				0x20,
+				0x65,
+				0x6C,
+				0x69,
+				0x74,
+			},
+			nil,
+		},
+		{
+			"empty list",
+			&Item{L: []*Item{}},
+			[]byte{0xc0},
+			nil,
+		},
+		{
+			"list of strings",
+			&Item{
+				L: []*Item{
+					&Item{D: []byte("cat")},
+					&Item{D: []byte("dog")},
+				},
+			},
+			[]byte{
+				0xc8, // 200
+				0x83, // 131
+				0x63, // c
+				0x61, // a
+				0x74, // t
+				0x83, // 131
+				0x64, // d
+				0x6f, // o
+				0x67, // g
+			},
+			nil,
+		},
+		{
+			"the set theoretical representation of three",
+			&Item{
+				L: []*Item{
+					&Item{L: []*Item{}},
+					&Item{L: []*Item{
+						&Item{L: []*Item{}},
+					}},
+					&Item{L: []*Item{
+						&Item{L: []*Item{}},
+						&Item{L: []*Item{
+							&Item{L: []*Item{}},
+						}},
+					}},
+				},
+			},
+			[]byte{
+				0xc7,
+				0xc0,
+				0xc1,
+				0xc0,
+				0xc3,
+				0xc0,
+				0xc1,
+				0xc0,
+			},
+			nil,
+		},
+	}
+	for _, tc := range cases {
+		got, err := Encode(tc.item)
+		if err != nil {
+			if !errors.Is(err, tc.err) {
+				t.Errorf("%s: want: %v got: %v", tc.desc, tc.err, err)
+			}
+		}
+		if !bytes.Equal(tc.want, got) {
+			t.Errorf("%s\nwant:\n%v\ngot:\n%v\n", tc.desc, tc.want, got)
+		}
+	}
+}
