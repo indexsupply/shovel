419ddbebe22375b841e2036d0e7dae2fd28aee31
Tommy Fan <tommy.fan@gusto.com>
2022-10-16 21:07:20 -0600

    finish up implementation

--- enr/enr.go
+++ enr/enr.go
@@ -1,25 +1,28 @@
 package enr
 
 import (
-	"strings"
-	"errors"
 	"encoding/base64"
-	"fmt"
+	"encoding/binary"
+	"errors"
+	"strings"
 
 	"github.com/indexsupply/lib/rlp"
 )
 
-type ENR struct{
-	ID string 
+type ENR struct {
+	Signature []byte
+	Sequence  uint64
+
+	ID        string
 	Secp256k1 []byte
 
-	Ip [4]byte
-	TcpPort int
-	UdpPort int
+	Ip      [4]byte
+	TcpPort uint16
+	UdpPort uint16
 
-	Ip6 [6]byte
-	Tcp6Port int
-	Udp6Port int
+	Ip6      [16]byte
+	Tcp6Port uint16
+	Udp6Port uint16
 }
 
 const enrTextPrefix = "enr:"
@@ -34,14 +37,94 @@ func FromTextEncoding(str string) (*ENR, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(len(b))
 
 	item, err := rlp.Decode(b)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("%s", item)
+	sig, seq, m, err := parseRLPEncoding(item)
+	if err != nil {
+		return nil, err
+	}
 
-	return &ENR{}, nil
+	var (
+		ip4 [4]byte
+		ip6 [16]byte
+	)
+	copy(ip4[:], m["ip"])
+	copy(ip6[:], m["ip6"])
+
+	return &ENR{
+		Signature: sig,
+		Sequence:  seq,
+		ID:        string(m["id"]),
+		Secp256k1: m["secp256k1"][:33],
+		Ip:        ip4,
+		Ip6:       ip6,
+		UdpPort:   binary.BigEndian.Uint16(leftPad(m["udp"], 2)),
+		TcpPort:   binary.BigEndian.Uint16(leftPad(m["tcp"], 2)),
+		Udp6Port:  binary.BigEndian.Uint16(leftPad(m["udp6"], 2)),
+		Tcp6Port:  binary.BigEndian.Uint16(leftPad(m["tcp6"], 2)),
+	}, nil
+}
+
+var (
+	ErrMissingENRKey    = errors.New("ENR is missing required key ID")
+	ErrMissingSignature = errors.New("ENR is missing a signature")
+	ErrMissingSequence  = errors.New("ENR is missing a sequence number")
+)
+
+// parseRLPEncoding returns a triple representing the signature,
+// sequence number, and a map of key-value pairs for the Node Record.
+// i must be the "root" RLP list that is in [signature, seq, k, v, ...] format
+// as specified in https://eips.ethereum.org/EIPS/eip-778.
+func parseRLPEncoding(i *rlp.Item) ([]byte, uint64, map[string][]byte, error) {
+	var (
+		sig []byte
+		seq uint64
+		m   map[string][]byte
+	)
+	switch listSize := len(i.L); {
+	case listSize < 1:
+		return sig, seq, m, ErrMissingSignature
+	case listSize < 2:
+		return sig, seq, m, ErrMissingSequence
+	case listSize < 4:
+		return sig, seq, m, ErrMissingENRKey
+	}
+	sig = i.L[0].D
+	// seq is expected to be a uint64 so we left pad to 8 bytes
+	s := i.L[1].D
+	seq = binary.BigEndian.Uint64(leftPad(s, 8))
+
+	m = make(map[string][]byte)
+	for idx := 2; idx < len(i.L); {
+		key := string(i.L[idx].D)
+		idx++
+		value := i.L[idx].D
+		idx++
+		m[key] = value
+	}
+
+	// "id" must be present in the key-value map
+	if _, ok := m["id"]; !ok {
+		return sig, seq, m, ErrMissingENRKey
+	}
+	if secp256k1Value, ok := m["secp256k1"]; ok && len(secp256k1Value) != 33 {
+		return sig, seq, m, errors.New("If secp256k1 is present, it must be exactly 33 bytes long.")
+	}
+
+	return sig, seq, m, nil
+}
+
+func leftPad(b []byte, wantedLength int) []byte {
+	if len(b) >= wantedLength {
+		return b
+	}
+	padded := make([]byte, wantedLength)
+	bytesNeeded := wantedLength - len(b)
+	copy(padded[bytesNeeded:], b)
+
+	return padded
 }
