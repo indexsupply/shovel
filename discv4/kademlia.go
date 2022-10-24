4be1c300f1b09231e88711f1f7c35efaa275d963
Tommy Fan <tommy.fan@gusto.com>
2022-10-23 18:31:12 -0700

    wip on find closest

--- discv4/kademlia.go
+++ discv4/kademlia.go
@@ -2,6 +2,7 @@ package discv4
 
 import (
 	"container/list"
+	"sort"
 	"sync"
 	"time"
 
@@ -16,6 +17,7 @@ const (
 )
 
 type kademliaTable struct {
+	mu       sync.Mutex // protects buckets
 	selfNode *enr.ENR
 	buckets  [bucketsCount]kBucket
 }
@@ -29,17 +31,22 @@ type bucketEntry struct {
 // most recently seen (head) to least recently seen (tail). The size
 // of the list is at most maxBucketSize.
 type kBucket struct {
-	mu         sync.Mutex // protects lru and entriesMap
 	lru        *list.List
 	entriesMap map[string]*list.Element
+}
+
+// nodes returns a slice of all the nodes (ENR) stored in this bucket.
+func (bucket *kBucket) nodes() []*enr.ENR {
+	var result []*enr.ENR
+	for element := bucket.lru.Front(); element != nil; element = element.Next() {
+		result = append(result, element.Value.(*bucketEntry).node)
+	}
+	return result
 }
 
 // Store inserts a node into this particular k-bucket. If the k-bucket is full,
 // then the least recently seen node is evicted.
 func (bucket *kBucket) store(node *enr.ENR) {
-	bucket.mu.Lock()
-	defer bucket.mu.Unlock()
-
 	if el, ok := bucket.entriesMap[node.NodeAddrHex()]; ok {
 		// cache hit; update
 		el.Value.(*bucketEntry).lastSeen = time.Now()
@@ -74,6 +81,9 @@ func newKademliaTable(selfNode *enr.ENR) *kademliaTable {
 // Inserts a node record into the Kademlia Table by putting it
 // in the appropriate k-bucket based on distance.
 func (kt *kademliaTable) Insert(node *enr.ENR) {
+	kt.mu.Lock()
+	defer kt.mu.Unlock()
+
 	distance := enr.LogDistance(kt.selfNode, node)
 	// In the unlikely event that the distance is closer than
 	// the mininum, put it in the closest bucket.
@@ -87,6 +97,37 @@ func (kt *kademliaTable) Insert(node *enr.ENR) {
 // It does a full table scan since the actual algorithm to do this is quite complex
 // and the table is not expected to be that large.
 func (kt *kademliaTable) FindClosest(target *enr.ENR, count int) []*enr.ENR {
-	// todo: implement
-	return nil
+	kt.mu.Lock()
+	defer kt.mu.Unlock()
+
+	s := &enrSorter{
+		nodes:  []*enr.ENR{},
+		target: target,
+	}
+	for _, b := range kt.buckets {
+		s.nodes = append(s.nodes, b.nodes()...)
+	}
+	sort.Sort(s)
+	return s.nodes[:count]
+}
+
+// Implement the sort.Interface for a slice of node records using
+// the xor distance metric from the target node as a way to compare.
+type enrSorter struct {
+	target *enr.ENR
+	nodes  []*enr.ENR
+}
+
+func (s *enrSorter) Len() int {
+	return len(s.nodes)
+}
+
+func (s *enrSorter) Less(i, j int) bool {
+	return enr.LogDistance(s.nodes[i], s.target) < enr.LogDistance(s.nodes[j], s.target)
+}
+
+func (s *enrSorter) Swap(i, j int) {
+	temp := s.nodes[i]
+	s.nodes[i] = s.nodes[j]
+	s.nodes[j] = temp
 }
