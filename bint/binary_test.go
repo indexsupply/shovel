6ff69420d68b1d9bd708f118ccd50e9f689d8978
Ryan Smith <r@32k.io>
2022-11-11 19:54:39 -0800

    bint: add controlled padding

--- bint/binary_test.go
+++ bint/binary_test.go
@@ -1,12 +1,15 @@
 package bint
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestDecode(t *testing.T) {
 	var cases = []uint8{0, 8, 16, 32, 64}
 	for _, e := range cases {
 		i := uint64(1<<e - 1)
-		b, n := Encode(i)
+		b, n := Encode(nil, i)
 		if n != e/8 {
 			t.Errorf("num bytes expected %d got: %d", e/8, n)
 		}
@@ -14,5 +17,41 @@ func TestDecode(t *testing.T) {
 		if got != i {
 			t.Errorf("expected %d got: %d", i, got)
 		}
+	}
+}
+
+func TestEncodeNil(t *testing.T) {
+	i := uint64(1<<16 - 1)
+	b, n := Encode(nil, i)
+	if n != 2 {
+		t.Errorf("expected %d to use 2 bytes. got: %d", i, n)
+	}
+	if !bytes.Equal(b, []byte{0xff, 0xff}) {
+		t.Errorf("expected %d to be encoded as: ffff got: %x", i, b)
+	}
+}
+
+func TestEncodeBuf(t *testing.T) {
+	b := make([]byte, 2)
+	i := uint64(1<<16 - 1)
+	_, n := Encode(b[:], i)
+	if n != 2 {
+		t.Errorf("expected %d to use 2 bytes. got: %d", i, n)
+	}
+	if !bytes.Equal(b, []byte{0xff, 0xff}) {
+		t.Errorf("expected %d to be encoded as: ffff got: %x", i, b)
+	}
+
+}
+
+func TestEncodeBuf_Pad(t *testing.T) {
+	b := make([]byte, 4)
+	i := uint64(1<<16 - 1)
+	_, n := Encode(b[:], i)
+	if n != 4 {
+		t.Errorf("expected %d to use 2 bytes. got: %d", i, n)
+	}
+	if !bytes.Equal(b, []byte{0xff, 0xff, 0x00, 0x00}) {
+		t.Errorf("expected %d to be encoded as: ffff got: %x", i, b)
 	}
 }
--- bint/bint.go
+++ bint/bint.go
@@ -1,22 +1,36 @@
 package bint
 
-// Returns unpadded, big endian representation of n
-func Encode(n uint64) ([]byte, uint8) {
+// Encodes a uint64 into a big-endian byte slice
+// To avoid an allocation, or to have a padded result,
+// supply an initialized value for b -otherwise use nil.
+func Encode(b []byte, n uint64) ([]byte, uint8) {
+	if b == nil {
+		b = make([]byte, size(n))
+	}
 	if n == 0 {
-		return []byte{}, 0
+		return b, 0
 	}
-	// Tommy's algorithm
-	var buf []byte
-	for i := n; i > 0; {
-		buf = append([]byte{byte(i & 0xff)}, buf...)
-		i = i >> 8
+	for i := uint64(0); n > 0; i++ {
+		b[i] = byte(n & 0xff)
+		n = n >> 8
 	}
-	return buf, uint8(len(buf))
+	return b, uint8(len(b))
 }
 
+func size(n uint64) uint8 {
+	var s uint8
+	for n > 0 {
+		n = n >> 8
+		s++
+	}
+	return s
+}
+
+// Decodes big-endian byte array into a uint64
+// Right-padded zero bytes are ignored.
 func Decode(b []byte) uint64 {
 	var n uint64
-	for i := range b {
+	for i := 0; i < len(b) && b[i] != 0; i++ {
 		n = n << 8
 		n += uint64(b[i])
 	}
