8bad8382efdf9704e1be5279d401c3dc8fe8898f
Tommy Fan <tommy.fan@gusto.com>
2022-11-15 18:55:19 -0700

    cleanup

--- rlpx/handshake_test.go
+++ rlpx/handshake_test.go
@@ -15,12 +15,8 @@ import (
 
 func TestHandshake(t *testing.T) {
 	tv := map[string]string{
-		"initiator_private_key":           "49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee",
-		"receiver_private_key":            "b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291",
-		"initiator_ephemeral_private_key": "869d6ecf5211f1cc60418a13b9d870b22959d0c16f02bec714c960dd2298a32d",
-		"receiver_ephemeral_private_key":  "e238eb8e04fee6511ab04c6dd3c89ce097b11f25d584863ac2b6d5b35b1847e4",
-		"initiator_nonce":                 "7e968bba13b6c50e2c4cd7f241cc0d64d1ac25c7f5952df231ac6a2bda8ee5d6",
-		"receiver_nonce":                  "b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291",
+		"initiator_private_key": "49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee",
+		"receiver_private_key":  "b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291",
 	}
 	ih, rh := setupHandshakes(t, tv)
 	initConn, recConn := net.Pipe()
@@ -73,21 +69,7 @@ func decodeHexString(t *testing.T, s string) []byte {
 
 func setupHandshakes(t *testing.T, tv map[string]string) (ih, rh *handshake) {
 	initPrvKey := secp256k1.PrivKeyFromBytes(decodeHexString(t, tv["initiator_private_key"]))
-	// initEphPrvKey := secp256k1.PrivKeyFromBytes(decodeHexString(t, tv["initiator_ephemeral_private_key"]))
-	// initNonce := decodeHexString(t, tv["initiator_nonce"])
-	// if len(initNonce) != 32 {
-	// 	t.Errorf("initiator nonce is not 32 bytes")
-	// }
-	// inonce := make([]byte, 32)
-	// copy(inonce[:], initNonce)
 	recPrvKey := secp256k1.PrivKeyFromBytes(decodeHexString(t, tv["receiver_private_key"]))
-	// recEphPrvKey := secp256k1.PrivKeyFromBytes(decodeHexString(t, tv["receiver_ephemeral_private_key"]))
-	// recNonce := decodeHexString(t, tv["receiver_nonce"])
-	// if len(recNonce) != 32 {
-	// 	t.Errorf("receiver nonce is not 32 bytes")
-	// }
-	// rnonce := make([]byte, 32)
-	// copy(rnonce[:], recNonce)
 	ih = newHandshake(initPrvKey, &enr.Record{PublicKey: recPrvKey.PubKey()})
 	rh = newHandshake(recPrvKey, &enr.Record{PublicKey: initPrvKey.PubKey()})
 	return
