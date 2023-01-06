f0c43f7f87f4996827adc476c8c232f3f571271d
Tommy Fan <tommy.fan@gusto.com>
2023-01-05 21:52:58 -0700

    manually write a match transfer function

---
+++ exp/erc721events/transfer.go
@@ -0,0 +1,46 @@
+package erc721events
+
+import (
+	"github.com/indexsupply/x/abi"
+)
+
+type Transfer struct {
+	To abi.Item
+	From abi.Item
+	TokenId abi.Item
+}
+
+var TransferEvent = abi.Event{
+	Name: "Transfer",
+	Type: "event",
+	Anonymous: false,
+	Inputs: []abi.Input{
+		{
+			Indexed: true,
+			Name: "To",
+			Type: "address",
+		},
+		{
+			Indexed: true,
+			Name: "From",
+			Type: "address",
+		},
+		{
+			Indexed: true,
+			Name: "TokenId",
+			Type: "uint256",
+		},
+	},
+}
+
+func MatchTransfer(l abi.Log) *Transfer {
+	items, matched := abi.Match(l, &TransferEvent)
+	if !matched {
+		return nil
+	}
+	return &Transfer{
+		To: items["To"],
+		From: items["From"],
+		TokenId: items["TokenId"],
+	}
+}
