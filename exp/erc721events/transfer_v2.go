fc69c61baed8113c220a971b2f83d991b41df3c1
Tommy Fan <tommy.fan@gusto.com>
2023-01-05 21:55:18 -0700

    v2

---
+++ exp/erc721events/transfer_v2.go
@@ -0,0 +1,24 @@
+package erc721events
+
+import (
+	"math/big"
+	"github.com/indexsupply/x/abi"
+)
+
+type TransferV2 struct {
+	To [20]byte
+	From [20]byte
+	TokenId *big.Int
+}
+
+func MatchTransferV2(l abi.Log) *TransferV2 {
+	items, matched := abi.Match(l, &TransferEvent)
+	if !matched {
+		return nil
+	}
+	return &TransferV2{
+		To: items["To"].Address(),
+		From: items["From"].Address(),
+		TokenId: items["TokenId"].BigInt(),
+	}
+}
