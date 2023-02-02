indexsupply/shovel

commit c6c2bfc0cac7d482cfcc11d24ba2334d59243b78

abi: bugfix for fixed length list decoding (#67)
This commit adds support for decoding fixed length lists. The solution
also improves the architecture of the code as it leverages tuple
decoding instead of doing it's own decoding. This means we can remove
the Size method on abit.Type. After all, the ABI spec instructs
implementations to treat lists as tuples.

abi/abi.go
@@ -360,20 +360,23 @@ func Decode(input []byte, t abit.Type) Item {
 		count := bint.Decode(input[:32])
 		return Item{Type: t, d: input[32 : 32+count]}
 	case abit.L:
-		n := 32
-		count := bint.Decode(input[:n])
-		items := make([]Item, count)
-		for i := uint64(0); i < count; i++ {
-			if t.Elem.Static() {
-				items[i] = Decode(input[n:], *t.Elem)
-				n += t.Elem.Size()
-				continue
+		switch t.Length {
+		case 0: //dynamic sized list
+			count := bint.Decode(input[:32])
+			types := make([]abit.Type, count)
+			for i := uint64(0); i < count; i++ {
+				types[i] = *t.Elem
 			}
-			offset := bint.Decode(input[n : n+32])
-			items[i] = Decode(input[32+offset:], *t.Elem)
-			n += 32
+			tuple := Decode(input[32:], abit.Tuple(types...))
+			return List(tuple.l...)
+		default: //fixed size list
+			types := make([]abit.Type, t.Length)
+			for i := uint(0); i < t.Length; i++ {
+				types[i] = *t.Elem
+			}
+			tuple := Decode(input, abit.Tuple(types...))
+			return ListK(t.Length, tuple.l...)
 		}
-		return List(items...)
 	case abit.T:
 		items := make([]Item, len(t.Fields))
 		for i, f := range t.Fields {

abi/abit/types.go
@@ -132,26 +132,6 @@ func (t Type) Static() bool {
 	return true
 }
 
-// Returns the number of bytes that the type will
-// occupy if the type is static.
-//
-// Returns 0 if the type is dynamic.
-func (t Type) Size() int {
-	if !t.Static() {
-		return 0
-	}
-	switch t.Kind {
-	case S:
-		return 32
-	case T:
-		return 32 * len(t.Fields)
-	case L:
-		return 32 * int(t.Length)
-	default:
-		return 0
-	}
-}
-
 var (
 	Address = Type{
 		Name: "address",

abi/abi_test.go
@@ -524,6 +524,11 @@ func TestDecode(t *testing.T) {
 			t:    abit.List(abit.String),
 		},
 		{
+			desc: "fixed size list of static types",
+			want: ListK(2, Uint8(42), Uint8(43)),
+			t:    abit.ListK(2, abit.Uint8),
+		},
+		{
 			desc: "tuple static",
 			want: Tuple(Uint64(0)),
 			t:    abit.Tuple(abit.Uint64),

abi/abit/types_test.go
@@ -5,46 +5,6 @@ import (
 	"testing"
 )
 
-func TestSize(t *testing.T) {
-	cases := []struct {
-		desc string
-		t    Type
-		want int
-	}{
-		{
-			desc: "simple static",
-			t:    Uint8,
-			want: 32,
-		},
-		{
-			desc: "fixed size list of fixed size types",
-			t:    ListK(2, Uint8),
-			want: 64,
-		},
-		{
-			desc: "dynamic sized list of static types",
-			t:    List(Uint8),
-			want: 0,
-		},
-		{
-			desc: "tuple with static fields",
-			t:    Tuple(Uint8),
-			want: 32,
-		},
-		{
-			desc: "tuple with dynamic fields",
-			t:    Tuple(Bytes),
-			want: 0,
-		},
-	}
-	for _, tc := range cases {
-		got := tc.t.Size()
-		if got != tc.want {
-			t.Errorf("%q got: %d want: %d", tc.desc, got, tc.want)
-		}
-	}
-}
-
 func TestStatic(t *testing.T) {
 	cases := []struct {
 		desc string

commit 69e5bb0b455b508266963d032d44369b9242b6e0

abi: bugfix. decode dynamic sized list with more than one element
There was a regression in #67 where we stopped checking the size
of an item after decoding it. This accumulated size is needed
when decoding nested tuples.

The Decode test was improved as to expose the particular bug.

Finally, the Encode test was simplified to make it easier to understand
how a nested tuple is encoded.

abi/abi.go
@@ -378,15 +378,17 @@ func Decode(input []byte, t abit.Type) Item {
 			return ListK(t.Length, tuple.l...)
 		}
 	case abit.T:
+		var n int
 		items := make([]Item, len(t.Fields))
 		for i, f := range t.Fields {
-			n := 32 * i
 			if f.Static() {
 				items[i] = Decode(input[n:], *f)
+				n += f.Size()
 				continue
 			}
 			offset := bint.Decode(input[n : n+32])
 			items[i] = Decode(input[offset:], *f)
+			n += 32
 		}
 		return Tuple(items...)
 	default:

abi/abit/types.go
@@ -132,6 +132,25 @@ func (t Type) Static() bool {
 	return true
 }
 
+// Returns number of bytes used by t if t is [Static]
+// Otherwise returns 0
+func (t Type) Size() int {
+	switch t.Kind {
+	case S:
+		return 32
+	case T:
+		var n int
+		for _, f := range t.Fields {
+			n += f.Size()
+		}
+		return n
+	case L:
+		return 32 * int(t.Length)
+	default:
+		return 0
+	}
+}
+
 var (
 	Address = Type{
 		Name: "address",

abi/abi_test.go
@@ -390,26 +390,25 @@ func TestEncode(t *testing.T) {
 		{
 			desc: "tuple with list of static tuples",
 			input: Tuple(
-				Uint8(42),
-				Uint8(43),
-				String("hello"),
 				List(
-					Tuple(
-						Uint8(42),
-						Uint8(43),
-					),
+					Tuple(Uint8(44), Uint8(45)),
+				),
+				List(
+					Tuple(Uint8(46), Uint8(47)),
+					Tuple(Uint8(48), Uint8(49)),
 				),
 			),
 			want: hb(`
-				000000000000000000000000000000000000000000000000000000000000002a
-				000000000000000000000000000000000000000000000000000000000000002b
-				0000000000000000000000000000000000000000000000000000000000000080
-				00000000000000000000000000000000000000000000000000000000000000c0
-				0000000000000000000000000000000000000000000000000000000000000005
-				68656c6c6f000000000000000000000000000000000000000000000000000000
+				0000000000000000000000000000000000000000000000000000000000000040
+				00000000000000000000000000000000000000000000000000000000000000a0
 				0000000000000000000000000000000000000000000000000000000000000001
-				000000000000000000000000000000000000000000000000000000000000002a
-				000000000000000000000000000000000000000000000000000000000000002b
+				000000000000000000000000000000000000000000000000000000000000002c
+				000000000000000000000000000000000000000000000000000000000000002d
+				0000000000000000000000000000000000000000000000000000000000000002
+				000000000000000000000000000000000000000000000000000000000000002e
+				000000000000000000000000000000000000000000000000000000000000002f
+				0000000000000000000000000000000000000000000000000000000000000030
+				0000000000000000000000000000000000000000000000000000000000000031
 			`),
 		},
 		{
@@ -578,21 +577,39 @@ func TestDecode(t *testing.T) {
 				),
 			),
 		},
+		{
+			desc: "tuple with list of tuples",
+			want: Tuple(
+				Uint8(42),
+				List(
+					Tuple(Uint8(43), Uint8(44)),
+				),
+				List(
+					Tuple(Uint8(45), Uint8(46)),
+					Tuple(Uint8(47), Uint8(48)),
+				),
+			),
+			t: abit.Tuple(
+				abit.Uint8,
+				abit.List(abit.Tuple(abit.Uint8, abit.Uint8)),
+				abit.List(abit.Tuple(abit.Uint8, abit.Uint8)),
+			),
+		},
 	}
 	for _, c := range cases {
-		got := Decode(debug(t, Encode(c.want)), c.t)
+		got := Decode(debug(c.desc, t, Encode(c.want)), c.t)
 		if !reflect.DeepEqual(c.want, got) {
 			t.Errorf("decode %q want: %#v got: %#v", c.desc, c.want, got)
 		}
 	}
 }
 
-func debug(t *testing.T, b []byte) []byte {
+func debug(desc string, t *testing.T, b []byte) []byte {
 	t.Helper()
 	out := fmt.Sprintf("len: %d\n", len(b))
 	for i := 0; i < len(b); i += 32 {
 		out += fmt.Sprintf("%x\n", b[i:i+32])
 	}
-	t.Logf("debug:\n%s\n", out)
+	t.Logf("%s\n%s\n", desc, out)
 	return b
 }

abi/abit/types_test.go
@@ -5,6 +5,56 @@ import (
 	"testing"
 )
 
+func TestSize(t *testing.T) {
+	cases := []struct {
+		desc string
+		t    Type
+		want int
+	}{
+		{
+			desc: "static",
+			t:    Uint8,
+			want: 32,
+		},
+		{
+			desc: "dynamic",
+			t:    Bytes,
+			want: 0,
+		},
+		{
+			desc: "tuple",
+			t:    Tuple(Uint8),
+			want: 32,
+		},
+		{
+			desc: "tuple with multiple fields",
+			t:    Tuple(Uint8, Uint8),
+			want: 64,
+		},
+		{
+			desc: "tuple with nested tuple",
+			t:    Tuple(Uint8, Uint8, Tuple(Uint8)),
+			want: 96,
+		},
+		{
+			desc: "dynamic size list",
+			t:    List(Uint8),
+			want: 0,
+		},
+		{
+			desc: "static size list",
+			t:    ListK(4, Uint8),
+			want: 128,
+		},
+	}
+	for _, tc := range cases {
+		got := tc.t.Size()
+		if got != tc.want {
+			t.Errorf("%q got: %d want: %d", tc.desc, got, tc.want)
+		}
+	}
+}
+
 func TestStatic(t *testing.T) {
 	cases := []struct {
 		desc string