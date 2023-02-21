b14a1f4a8a8bd0279b3b16e9eabf83367078ba4c
Ryan Smith <r@32k.io>
2023-02-21 08:57:50 -0800

    genabi: unit test listHelper methods

--- genabi/gen_test.go
+++ genabi/gen_test.go
@@ -6,6 +6,103 @@ import (
 	"kr.dev/diff"
 )
 
+func TestHasNext(t *testing.T) {
+	cases := []struct {
+		lh   listHelper
+		want bool
+	}{
+		{
+			lh:   listHelper{Input: Input{Type: "uint8"}},
+			want: false,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][][]"}, Index: 0},
+			want: true,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][][]"}, Index: 1},
+			want: true,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][][]"}, Index: 2},
+			want: false,
+		},
+	}
+	for _, tc := range cases {
+		diff.Test(t, t.Errorf, tc.lh.HasNext(), tc.want)
+	}
+}
+
+func TestFixedLength(t *testing.T) {
+	cases := []struct {
+		lh   listHelper
+		want bool
+	}{
+		{
+			lh:   listHelper{Input: Input{Type: "uint8"}},
+			want: false,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[]"}},
+			want: false,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[2]"}},
+			want: true,
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[200]"}},
+			want: true,
+		},
+	}
+	for _, tc := range cases {
+		diff.Test(t, t.Errorf, tc.lh.FixedLength(), tc.want)
+	}
+}
+
+func TestMakeArg(t *testing.T) {
+	cases := []struct {
+		lh   listHelper
+		want string
+	}{
+		{
+			lh:   listHelper{Input: Input{Type: "uint8"}},
+			want: "uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[]"}},
+			want: "[]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[2]"}},
+			want: "[2]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[2][3]"}},
+			want: "[3][2]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][]"}},
+			want: "[][]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][]"}, Index: 0},
+			want: "[][]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][]"}, Index: 1},
+			want: "[]uint8",
+		},
+		{
+			lh:   listHelper{Input: Input{Type: "uint8[][]"}, Index: 2},
+			want: "uint8",
+		},
+	}
+	for _, tc := range cases {
+		diff.Test(t, t.Errorf, tc.lh.MakeArg(), tc.want)
+	}
+}
+
 func TestLower(t *testing.T) {
 	cases := []struct {
 		input string
