indexsupply/shovel

commit 647880db3bcf3f4c05231a1fb17d27579234533b

rlp: alloc-free scanner (#107)
This commit makes a big change to how RLP data is encoded and decoded.
Instead of using an Item struct, the API now works directly with []byte
and [][]byte slices. The API no longer decodes arbitrarily nested data
structures and instead relies on the caller to successively create
Iterators. This allows callers to optimize their usage as to reduce
memory and computation utilization.

    BenchmarkDecode-10 486744081 2.452 ns/op 0 B/op 0 allocs/op

rlp/rlp.go
@@ -5,7 +5,7 @@
 package rlp
 
 import (
-	"errors"
+	"bytes"
 
 	"github.com/indexsupply/x/bint"
 )
@@ -18,62 +18,31 @@ const (
 	listNL, listNH   byte = 248, 255
 )
 
-func List(items ...Item) Item {
-	if items == nil {
-		items = []Item{}
+func Encode(input []byte) []byte {
+	switch n := len(input); {
+	case n == 1 && input[0] == 0:
+		return []byte{0x80}
+	case n == 1 && input[0] <= str1H:
+		return input
+	case n <= 55:
+		return append(
+			[]byte{str55L + byte(n)},
+			input...,
+		)
+	default:
+		length, lengthSize := encodeLength(len(input))
+		header := append(
+			[]byte{str55H + lengthSize},
+			length...,
+		)
+		return append(header, input...)
 	}
-	return Item{l: items}
 }
 
-func (i Item) At(pos int) Item {
-	if len(i.l) < pos {
-		return Item{}
-	}
-	return i.l[pos]
-}
-
-func (i Item) List() []Item {
-	return i.l
-}
-
-// Instead of using standard data types and reflection
-// this package chooses to encode Items.
-// Set d or l but not both.
-// l is a list of Item for arbitrarily nested lists.
-// d is the data payload for the item.
-type Item struct {
-	d []byte
-	l []Item
-}
-
-func Encode(input Item) []byte {
-	if input.d != nil && input.l != nil {
-		panic("must set d xor l")
-	}
-	if input.d != nil {
-		switch n := len(input.d); {
-		case n == 1 && input.d[0] == 0:
-			return []byte{0x80}
-		case n == 1 && input.d[0] <= str1H:
-			return input.d
-		case n <= 55:
-			return append(
-				[]byte{str55L + byte(n)},
-				input.d...,
-			)
-		default:
-			length, lengthSize := encodeLength(len(input.d))
-			header := append(
-				[]byte{str55H + lengthSize},
-				length...,
-			)
-			return append(header, input.d...)
-		}
-	}
-
+func EncodeList(inputs ...[]byte) []byte {
 	var out []byte
-	for _, l := range input.l {
-		out = append(out, Encode(l)...)
+	for _, input := range inputs {
+		out = append(out, Encode(input)...)
 	}
 	if len(out) <= 55 {
 		return append(
@@ -105,89 +74,109 @@ func decodeLength(t byte, input []byte) (int, int) {
 	return int(n), int(l)
 }
 
-var (
-	errNoBytes     = errors.New("input has no bytes")
-	errTooFewBytes = errors.New("input has fewer bytes than specified by header")
-)
-
-func Decode(input []byte) (Item, error) {
-	if len(input) == 0 {
-		return Item{}, errNoBytes
-	}
+// Removes RLP encoding data and returns the encoded bytes
+// See [Iterator] for decoding a list of RLP data
+func Bytes(input []byte) []byte {
 	switch {
+	case len(input) == 0:
+		return nil
+	case bytes.Equal(input, []byte{0x80}):
+		return nil
 	case input[0] <= str1H:
-		return Item{d: []byte{input[0]}}, nil
+		return input[0:1]
 	case input[0] <= str55H:
 		i, n := 1, int(input[0]-str55L)
 		if len(input) < i+n {
-			return Item{}, errTooFewBytes
+			return nil
 		}
-		return Item{d: input[i : i+n]}, nil
+		return input[i : i+n]
 	case input[0] <= strNH:
 		i, n := decodeLength(str55H, input)
 		if len(input) < i+n {
-			return Item{}, errTooFewBytes
+			return nil
 		}
-		return Item{d: input[i : i+n]}, nil
+		return input[i : i+n]
 	default:
-		// The first byte indicates a list
-		// and if the first byte is >= 248 (listNL)
-		// then the list has a length > 55 and
-		// therefore the next (input[0]-247 (list55H))
-		// bytes will describe the length of the list.
-		// We advance the cursor i past the length description.
-		// We also compute the size of the list and check the input
-		// to ensure the size matches the header's length description.
-		var i, listSize int
-		switch {
-		case input[0] <= list55H:
-			i, listSize = 1, int(input[0]-list55L)
-		case input[0] <= listNH:
-			i, listSize = decodeLength(list55H, input)
-		}
-
-		switch {
-		case len(input[i:]) < listSize:
-			return Item{}, errTooFewBytes
-		case len(input[i:]) > listSize:
-			// It's possible that the input contains
-			// more bytes that is specified by the
-			// header's length. In this case, instead
-			// of returning an error, we simply remove
-			// the extra bytes.
-			input = input[:i+listSize]
-		}
-
-		item := Item{l: []Item{}}
-		for i < len(input) {
-			var headerSize, payloadSize int
-			switch {
-			case input[i] <= str1H:
-				headerSize = 0
-				payloadSize = 1
-			case input[i] <= str55H:
-				headerSize = 1
-				payloadSize = int(input[i] - str55L)
-			case input[i] <= strNH:
-				headerSize, payloadSize = decodeLength(str55H, input[i:])
-			case input[i] <= list55H:
-				headerSize = 1
-				payloadSize = int(input[i] - list55L)
-			default:
-				headerSize, payloadSize = decodeLength(list55H, input[i:])
-			}
-
-			if int(i+headerSize+payloadSize) > len(input) {
-				return Item{}, errTooFewBytes
-			}
-
-			d, err := Decode(input[i : i+headerSize+payloadSize])
-			if err != nil {
-				return Item{}, err
-			}
-			item.l = append(item.l, d)
-			i += headerSize + payloadSize
-		}
-		return item, nil
+		return input
 	}
+}
+
+// For iterating over an RLP list
+type Iterator struct {
+	i    int
+	data []byte
+}
+
+// Returns a new iter. Input is assumed to be
+// raw RLP bytes. The input size bytes are removed
+// from the beginning of input and trailing bytes
+// are removed.
+//
+// A nil [Iterator] is returned when the
+// input doesn't contain a list or
+// the list data is corrupt.
+func Iter(input []byte) *Iterator {
+	if len(input) == 0 {
+		return nil
+	}
+	if input[0] <= strNH { // not a list
+		return nil
+	}
+	var i, listSize int
+	switch {
+	case input[0] <= list55H:
+		i, listSize = 1, int(input[0]-list55L)
+	case input[0] <= listNH:
+		i, listSize = decodeLength(list55H, input)
+	}
+	if len(input[i:]) < listSize {
+		return nil
+	}
+	//ignore bytes beyond listSize
+	return &Iterator{data: input[i : i+listSize]}
+}
+
+func size(input []byte) int {
+	var headerSize, payloadSize int
+	switch {
+	case input[0] <= str1H:
+		headerSize = 0
+		payloadSize = 1
+	case input[0] <= str55H:
+		headerSize = 1
+		payloadSize = int(input[0] - str55L)
+	case input[0] <= strNH:
+		headerSize, payloadSize = decodeLength(str55H, input[0:])
+	case input[0] <= list55H:
+		headerSize = 1
+		payloadSize = int(input[0] - list55L)
+	default:
+		headerSize, payloadSize = decodeLength(list55H, input[0:])
+	}
+	return headerSize + payloadSize
+}
+
+func (it *Iterator) HasNext() bool {
+	return len(it.data[it.i:]) > 0
+}
+
+// Moves the iter forward by one
+// and calls [Bytes] on the underlying value
+// Returns nil if there is no more data to scan
+// of if the RLP size header is corrupt
+func (it *Iterator) Bytes() []byte {
+	if it.i > len(it.data) {
+		return nil
+	}
+	data := it.data[it.i:]
+	ln := len(data)
+	if ln == 0 {
+		return nil
+	}
+	sz := size(data)
+	if ln < sz {
+		return nil
+	}
+	it.i += sz
+	return Bytes(data[:sz])
 }

rlp/rlp_test.go
@@ -1,115 +1,25 @@
 package rlp
 
 import (
-	"bytes"
-	"crypto/rand"
-	"errors"
-	"fmt"
-	"reflect"
+	"encoding/hex"
 	"testing"
 
-	"github.com/indexsupply/x/tc"
+	"github.com/indexsupply/x/bint"
+	"kr.dev/diff"
 )
 
-func ExampleEncode() {
-	item := List(
-		String("foo"),
-		List(String("bar"), String("baz")),
-	)
-	item, _ = Decode(Encode(item))
-
-	var (
-		first  = item.At(0).String()
-		second = item.At(1).At(0).String()
-		third  = item.At(1).At(1).String()
-	)
-	fmt.Println(first, second, third)
-
-	//Output:
-	// foo bar baz
-}
-
-func FuzzEncode(f *testing.F) {
-	var (
-		numItems uint64 = 10
-		payload         = []byte("hello")
-	)
-	f.Add(numItems, payload)
-	f.Fuzz(func(t *testing.T, n uint64, d []byte) {
-		var items []Item
-		for i := 0; i < int(n); i++ {
-			items = append(items, Bytes(d))
-		}
-		item := List(items...)
-		got, err := Decode(Encode(item))
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !reflect.DeepEqual(item, got) {
-			t.Errorf("want:\n%v\ngot:\n%v\n", item, got)
-		}
-	})
-}
-
 func BenchmarkEncode(b *testing.B) {
-	payload := []byte("hello world")
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
-		Encode(Bytes(payload))
+		Encode([]byte("hello world"))
 	}
 }
 
-func randBytes(n int) []byte {
-	res := make([]byte, n)
-	rand.Read(res)
-	return res
-}
-
-func TestDecode_Padding(t *testing.T) {
-	exp := []byte{0x01, 0x00}
-	b := Encode(List(Bytes(exp)))
-	i, err := Decode(append(b, exp...))
-	tc.NoErr(t, err)
-	got := i.At(0).Bytes()
-	if !bytes.Equal(exp, got) {
-		t.Errorf("expected: %x got: %x", exp, got)
-	}
-}
-
-func TestDecode_Errors(t *testing.T) {
-	cases := []struct {
-		desc  string
-		input []byte
-		err   error
-	}{
-		{
-			"short string no error",
-			[]byte{byte(1)},
-			nil,
-		},
-		{
-			"long string. too few bytes",
-			append(
-				[]byte{
-					byte(str55H + 1),
-					byte(56),
-				},
-				randBytes(55)...,
-			),
-			errTooFewBytes,
-		},
-	}
-	for _, tc := range cases {
-		_, err := Decode(tc.input)
-		if tc.err == nil {
-			if err != nil {
-				t.Errorf("expected nil error got: %v", err)
-			}
-		} else {
-			if !errors.Is(tc.err, err) {
-				t.Errorf("expected %v got %v", tc.err, err)
-			}
-		}
+func BenchmarkDecode(b *testing.B) {
+	eb := Encode([]byte("hello world"))
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		Bytes(eb)
 	}
 }
 
@@ -162,167 +72,141 @@ func TestDecodeLength(t *testing.T) {
 	}
 }
 
-func TestDecodeZero(t *testing.T) {
-	item, err := Decode([]byte{0x80})
-	tc.NoErr(t, err)
-	if item.Uint16() != 0 {
-		t.Errorf("want: 0 got: %d", item.Uint16())
-	}
-	if item.Uint64() != 0 {
-		t.Errorf("want: 0 got: %d", item.Uint64())
-	}
-}
-
 func TestDecode(t *testing.T) {
 	cases := []struct {
-		desc string
-		item Item
+		desc  string
+		input []byte
 	}{
 		{
 			"empty bytes",
-			Bytes(nil),
+			nil,
 		},
 		{
 			"short string",
-			String("a"),
+			[]byte("foo"),
 		},
 		{
 			"long string",
-			String("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
-		},
-		{
-			"empty list",
-			List(),
-		},
-		{
-			"list of short strings",
-			List(String("a"), String("b")),
-		},
-		{
-			"list of long strings",
-			List(
-				String("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
-				String("Porem ipsum dolor sit amet, consectetur adipisicing elit"),
-			),
-		},
-		{
-			"the set theoretical representation of three",
-			List(
-				List(),
-				List(List()),
-				List(List(), List(List())),
-			),
+			[]byte("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
 		},
 	}
 	for _, tc := range cases {
-		got, err := Decode(Encode(tc.item))
-		if err != nil {
-			t.Errorf("error %s: %s", tc.desc, err)
+		diff.Test(t, t.Errorf, tc.input, Bytes(Encode(tc.input)))
+	}
+}
+
+func TestDecode_List(t *testing.T) {
+	cases := []struct {
+		desc  string
+		input [][]byte
+	}{
+		{
+			"empty list",
+			[][]byte{},
+		},
+		{
+			"list of short strings",
+			[][]byte{
+				[]byte("foo"),
+				[]byte("bar"),
+			},
+		},
+		{
+			"list of long strings",
+			[][]byte{
+				[]byte("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+				[]byte("Porem ipsum dolor sit amet, consectetur adipisicing elit"),
+			},
+		},
+	}
+	for _, tc := range cases {
+		itr := Iter(EncodeList(tc.input...))
+		res := [][]byte{}
+		for itr.HasNext() {
+			res = append(res, itr.Bytes())
 		}
-		if !reflect.DeepEqual(tc.item, got) {
-			t.Errorf("%s\nwant:\n%# v\ngot:\n%# v\n", tc.desc, tc.item, got)
+		diff.Test(t, t.Errorf, tc.input, res)
+	}
+}
+
+func TestDecode_List_Nested(t *testing.T) {
+	// Set-theoretic definition of 3
+	// [ [], [[]], [ [], [[]] ] ]
+	var (
+		zero  = EncodeList([]byte{})
+		one   = EncodeList(zero)
+		two   = EncodeList(zero, one)
+		input = EncodeList(zero, one, two)
+	)
+	for i, s := 0, Iter(input); s.HasNext(); i++ {
+		switch i {
+		case 0:
+			s0 := Iter(s.Bytes())
+			diff.Test(t, t.Errorf, []byte(nil), s0.Bytes())
+		case 1:
+			s1 := Iter(s.Bytes())
+			s2 := Iter(s1.Bytes())
+			diff.Test(t, t.Errorf, []byte(nil), s2.Bytes())
+		case 2:
+			s1 := Iter(s.Bytes())
+			s2 := Iter(s1.Bytes())
+			diff.Test(t, t.Errorf, []byte(nil), s2.Bytes())
+			s3 := Iter(s1.Bytes())
+			s4 := Iter(s3.Bytes())
+			diff.Test(t, t.Errorf, []byte(nil), s4.Bytes())
 		}
 	}
+}
+
+func hb(s string) []byte {
+	b, _ := hex.DecodeString(s)
+	return b
 }
 
 func TestEncode(t *testing.T) {
 	cases := []struct {
-		desc string
-		item Item
-		want []byte
+		desc  string
+		input []byte
+		want  []byte
 	}{
 		{
 			"zero byte",
-			Byte(0),
-			[]byte{0x80},
-		},
-		{
-			"int 0",
-			Int(0),
+			[]byte{0},
 			[]byte{0x80},
 		},
 		{
 			"int 1024",
-			Int(1024),
+			bint.Encode(nil, 1<<10),
 			[]byte{0x82, 0x04, 0x00},
 		},
 		{
-			"empty string",
-			String(""),
-			[]byte{0x80},
+			"long string",
+			[]byte("foobarbazfoobarbazfoobarbazfoobarbazfoobarbazfoobarbaz"),
+			hb("b6666f6f62617262617a666f6f62617262617a666f6f62617262617a666f6f62617262617a666f6f62617262617a666f6f62617262617a"),
 		},
-		{
-			"non-empty string",
-			String("Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
-			[]byte{
-				0xB8,
-				0x38,
-				0x4C,
-				0x6F,
-				0x72,
-				0x65,
-				0x6D,
-				0x20,
-				0x69,
-				0x70,
-				0x73,
-				0x75,
-				0x6D,
-				0x20,
-				0x64,
-				0x6F,
-				0x6C,
-				0x6F,
-				0x72,
-				0x20,
-				0x73,
-				0x69,
-				0x74,
-				0x20,
-				0x61,
-				0x6D,
-				0x65,
-				0x74,
-				0x2C,
-				0x20,
-				0x63,
-				0x6F,
-				0x6E,
-				0x73,
-				0x65,
-				0x63,
-				0x74,
-				0x65,
-				0x74,
-				0x75,
-				0x72,
-				0x20,
-				0x61,
-				0x64,
-				0x69,
-				0x70,
-				0x69,
-				0x73,
-				0x69,
-				0x63,
-				0x69,
-				0x6E,
-				0x67,
-				0x20,
-				0x65,
-				0x6C,
-				0x69,
-				0x74,
-			},
-		},
+	}
+	for _, tc := range cases {
+		diff.Test(t, t.Errorf, tc.want, Encode(tc.input))
+	}
+}
+
+func TestEncode_List(t *testing.T) {
+	cases := []struct {
+		desc  string
+		input [][]byte
+		want  []byte
+	}{
 		{
 			"empty list",
-			List(),
+			nil,
 			[]byte{0xc0},
 		},
 		{
 			"list of strings",
-			List(String("cat"), String("dog")),
+			[][]byte{
+				[]byte("cat"),
+				[]byte("dog"),
+			},
 			[]byte{
 				0xc8, // 200
 				0x83, // 131
@@ -335,29 +219,8 @@ func TestEncode(t *testing.T) {
 				0x67, // g
 			},
 		},
-		{
-			"the set theoretical representation of three",
-			List(
-				List(),
-				List(List()),
-				List(List(), List(List())),
-			),
-			[]byte{
-				0xc7,
-				0xc0,
-				0xc1,
-				0xc0,
-				0xc3,
-				0xc0,
-				0xc1,
-				0xc0,
-			},
-		},
 	}
 	for _, tc := range cases {
-		got := Encode(tc.item)
-		if !bytes.Equal(tc.want, got) {
-			t.Errorf("%s\nwant:\n%v\ngot:\n%v\n", tc.desc, tc.want, got)
-		}
+		diff.Test(t, t.Errorf, tc.want, EncodeList(tc.input...))
 	}
 }

bint/bint.go
@@ -1,6 +1,8 @@
 // big endian, uint64 binary encoding/decoding
 package bint
 
+import "github.com/holiman/uint256"
+
 // Encodes a uint64 into a big-endian byte slice
 // To avoid an allocation, or to have a padded result,
 // supply an initialized value for b -otherwise use nil.
@@ -41,4 +43,14 @@ func Decode(b []byte) uint64 {
 		n += uint64(b[i])
 	}
 	return n
+}
+
+func Uint16(b []byte) uint16 { return uint16(Decode(b)) }
+func Uint32(b []byte) uint32 { return uint32(Decode(b)) }
+func Uint64(b []byte) uint64 { return uint64(Decode(b)) }
+
+func Uint256(b []byte) uint256.Int {
+	var i uint256.Int
+	i.SetBytes32(b)
+	return i
 }

discv4/discover.go
@@ -8,12 +8,13 @@ import (
 	"sync"
 	"time"
 
+	"github.com/indexsupply/x/bint"
 	"github.com/indexsupply/x/discv4/kademlia"
 	"github.com/indexsupply/x/enr"
 	"github.com/indexsupply/x/isxerrors"
 	"github.com/indexsupply/x/isxhash"
-	"github.com/indexsupply/x/isxsecp256k1"
 	"github.com/indexsupply/x/rlp"
+	"github.com/indexsupply/x/wsecp256k1"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
@@ -104,11 +105,9 @@ func (p *process) serve(uaddr *net.UDPAddr, packet []byte) error {
 	if !bytes.Equal(packet[:hashSize], isxhash.Keccak(packet[hashSize:])) {
 		return errors.New("packet contains invalid hash")
 	}
-	var sig [65]byte
-	copy(sig[:], packet[hashSize:hashSize+sigSize])
-	fromPubkey, err := isxsecp256k1.Recover(
-		sig,
-		isxhash.Keccak32(packet[hashSize+sigSize:]),
+	fromPubkey, err := wsecp256k1.Recover(
+		packet[hashSize:hashSize+sigSize],
+		isxhash.Keccak(packet[hashSize+sigSize:]),
 	)
 	if err != nil {
 		return errors.New("unable to extract pubkey from packet")
@@ -139,11 +138,8 @@ func (p *process) serve(uaddr *net.UDPAddr, packet []byte) error {
 
 func (p *process) handleENRRequest(req *enr.Record, packet []byte) error {
 	// packet-data = [request-hash, ENR]
-	item, err := rlp.Decode(packet[headerSize:])
-	if err != nil {
-		return err
-	}
-	expiration := item.At(0).Time()
+	t := bint.Uint32(rlp.Bytes(packet[headerSize:]))
+	expiration := time.Unix(int64(t), 0)
 	if expiration.Before(time.Now()) {
 		return errors.New("expired enr request")
 	}
@@ -151,35 +147,32 @@ func (p *process) handleENRRequest(req *enr.Record, packet []byte) error {
 	if err != nil {
 		return err
 	}
-	_, err = p.write(0x06, req.UDPAddr(), rlp.List(
-		rlp.Bytes(isxhash.Keccak(packet)),
-		rlp.Bytes(rec),
+	_, err = p.write(0x06, req.UDPAddr(), rlp.EncodeList(
+		isxhash.Keccak(packet),
+		rec,
 	))
 	return err
 }
 
 func (p *process) handleFindNode(req *enr.Record, packet []byte) error {
 	// packet-data = [target, expiration, ...]
-	item, err := rlp.Decode(packet[headerSize:])
-	if err != nil {
-		return err
-	}
 	var (
-		recs  = p.ktable.FindClosest(isxhash.Keccak32(item.At(0).Bytes()), 16)
-		nodes []rlp.Item
+		target = rlp.Bytes(packet[headerSize:])
+		recs   = p.ktable.FindClosest(isxhash.Keccak32(target), 16)
+		nodes  [][]byte
 	)
 	for _, rec := range recs {
-		id := isxsecp256k1.Encode(rec.PublicKey)
-		nodes = append(nodes, rlp.List(
-			rlp.Bytes(rec.Ip),
-			rlp.Uint16(rec.UdpPort),
-			rlp.Uint16(rec.TcpPort),
-			rlp.Bytes(id[:]),
+		id := wsecp256k1.Encode(rec.PublicKey)
+		nodes = append(nodes, rlp.EncodeList(
+			rec.Ip,
+			bint.Encode(nil, uint64(rec.UdpPort)),
+			bint.Encode(nil, uint64(rec.TcpPort)),
+			id,
 		))
 	}
-	_, err = p.write(0x04, req.UDPAddr(), rlp.List(
-		rlp.List(nodes...),
-		rlp.Time(time.Now().Add(time.Hour)),
+	_, err := p.write(0x04, req.UDPAddr(), rlp.EncodeList(
+		rlp.EncodeList(nodes...),
+		bint.Encode(nil, uint64(time.Now().Add(time.Hour).Unix())),
 	))
 	return err
 }
@@ -187,28 +180,21 @@ func (p *process) handleFindNode(req *enr.Record, packet []byte) error {
 func (p *process) handleNeighbors(req *enr.Record, packet []byte) error {
 	// packet-data = [nodes, expiration, ...]
 	// nodes = [[ip, udp-port, tcp-port, node-id], ...]
-	pd := packet[headerSize:]
-	item, err := rlp.Decode(pd)
-	if err != nil {
-		return err
-	}
 	var (
-		nodes   = item.At(0)
+		pd      = packet[headerSize:]
+		itr     = rlp.Iter(pd)
 		records []*enr.Record
 	)
-	for i := 0; i < len(nodes.List()); i++ {
+	for s := rlp.Iter(itr.Bytes()); s.HasNext(); {
 		var (
-			node = nodes.At(i)
+			node = rlp.Iter(s.Bytes())
 			rec  = &enr.Record{}
 			err  error
 		)
-		rec.Ip, err = node.At(0).IP()
-		if err != nil {
-			return err
-		}
-		rec.UdpPort = node.At(1).Uint16()
-		rec.TcpPort = node.At(2).Uint16()
-		rec.PublicKey, err = node.At(3).Secp256k1PublicKey()
+		rec.Ip = net.IP(node.Bytes())
+		rec.UdpPort = bint.Uint16(node.Bytes())
+		rec.TcpPort = bint.Uint16(node.Bytes())
+		rec.PublicKey, err = wsecp256k1.Decode(node.Bytes())
 		if err != nil {
 			return isxerrors.Errorf("reading pubkey: %w", err)
 		}
@@ -237,24 +223,21 @@ func (p *process) handlePing(req *enr.Record, packet []byte) error {
 		hash = packet[:hashSize]
 		pd   = packet[headerSize:]
 	)
-	item, err := rlp.Decode(pd)
-	if err != nil {
-		return err
-	}
-	reqFrom, err := item.At(1).At(0).IP()
-	if err != nil {
-		return errors.New("malformed ping from data")
-	}
+	itr := rlp.Iter(pd)
+	itr.Bytes() //skip
+	frm := rlp.Iter(itr.Bytes())
+
+	reqFrom := net.IP(frm.Bytes())
 	if !reqFrom.Equal(req.Ip) {
 		return errors.New("packet ip address doesn't match udp")
 	}
-	reqFromPort := item.At(1).At(1).Uint16()
+	reqFromPort := bint.Uint16(frm.Bytes())
 	if reqFromPort != req.UdpPort {
 		return errors.New("mismatch ping from-port with udp packet")
 	}
 	p.log("<ping: %s %x\n", req, hash[:4])
 
-	err = p.Pong(hash, req)
+	err := p.Pong(hash, req)
 	if err != nil {
 		return err
 	}
@@ -280,14 +263,10 @@ func (p *process) handlePing(req *enr.Record, packet []byte) error {
 
 func (p *process) handlePong(req *enr.Record, packet []byte) error {
 	// packet-data = [to, ping-hash, expiration, enr-seq, ...]
-	item, err := rlp.Decode(packet[headerSize:])
-	if err != nil {
-		return err
-	}
-	hash, err := item.At(1).Hash()
-	if err != nil {
-		return err
-	}
+	pd := packet[headerSize:]
+	itr := rlp.Iter(pd)
+	itr.Bytes() //skip
+	hash := itr.Bytes()
 
 	p.log("<pong: %s %x\n", req, hash[:4])
 
@@ -321,12 +300,11 @@ func (p *process) handlePong(req *enr.Record, packet []byte) error {
 // - packet-header = hash || signature || packet-type
 // - hash = keccak256(signature || packet-type || packet-data)
 // - signature = sign(packet-type || packet-data)
-func (p *process) write(pt byte, to *net.UDPAddr, it rlp.Item) ([]byte, error) {
-	pd := rlp.Encode(it)
+func (p *process) write(pt byte, to *net.UDPAddr, data []byte) ([]byte, error) {
 	var ts []byte
 	ts = append(ts, pt)
-	ts = append(ts, pd...)
-	sig, err := isxsecp256k1.Sign(p.prv, isxhash.Keccak32(ts))
+	ts = append(ts, data...)
+	sig, err := wsecp256k1.Sign(p.prv, isxhash.Keccak(ts))
 	if err != nil {
 		return nil, err
 	}
@@ -334,7 +312,7 @@ func (p *process) write(pt byte, to *net.UDPAddr, it rlp.Item) ([]byte, error) {
 	var th, hash []byte
 	th = append(th, sig[:]...)
 	th = append(th, pt)
-	th = append(th, pd...)
+	th = append(th, data...)
 	hash = isxhash.Keccak(th)
 
 	var header []byte
@@ -342,30 +320,29 @@ func (p *process) write(pt byte, to *net.UDPAddr, it rlp.Item) ([]byte, error) {
 	header = append(header, sig[:]...)
 	header = append(header, pt)
 
-	packet := append(header, pd...)
+	packet := append(header, data...)
 	_, err = p.conn.WriteTo(packet, to)
 	return hash, err
 }
 
 func (p *process) FindNode(target *secp256k1.PublicKey, dest *enr.Record) error {
-	tb := isxsecp256k1.Encode(target)
-	_, err := p.write(0x03, dest.UDPAddr(), rlp.List(
-		rlp.Bytes(tb[:]),
-		rlp.Time(time.Now().Add(time.Hour)),
+	_, err := p.write(0x03, dest.UDPAddr(), rlp.EncodeList(
+		wsecp256k1.Encode(target),
+		bint.Encode(nil, uint64(time.Now().Add(time.Hour).Unix())),
 	))
-	p.log(">find: %s %x\n", dest, tb[:4])
+	p.log(">find: %s\n", dest)
 	return err
 }
 
 func (p *process) Pong(pingHash []byte, dest *enr.Record) error {
-	_, err := p.write(0x02, dest.UDPAddr(), rlp.List(
-		rlp.List(
-			rlp.Bytes(dest.Ip),
-			rlp.Uint16(dest.UdpPort),
-			rlp.Uint16(dest.TcpPort),
+	_, err := p.write(0x02, dest.UDPAddr(), rlp.EncodeList(
+		rlp.EncodeList(
+			dest.Ip,
+			bint.Encode(nil, uint64(dest.UdpPort)),
+			bint.Encode(nil, uint64(dest.TcpPort)),
 		),
-		rlp.Bytes(pingHash),
-		rlp.Time(time.Now().Add(time.Hour)),
+		pingHash,
+		bint.Encode(nil, uint64(time.Now().Add(time.Hour).Unix())),
 	))
 	p.log(">pong: %s\n", dest)
 	return err
@@ -380,19 +357,19 @@ func (p *process) Ping(dest *enr.Record) error {
 		return nil
 	}
 
-	h, err := p.write(0x01, dest.UDPAddr(), rlp.List(
-		rlp.Byte(4),
-		rlp.List(
-			rlp.Bytes(p.self.Ip),
-			rlp.Uint16(p.self.UdpPort),
-			rlp.Uint16(p.self.TcpPort),
+	h, err := p.write(0x01, dest.UDPAddr(), rlp.EncodeList(
+		[]byte{4},
+		rlp.EncodeList(
+			p.self.Ip,
+			bint.Encode(nil, uint64(p.self.UdpPort)),
+			bint.Encode(nil, uint64(p.self.TcpPort)),
 		),
-		rlp.List(
-			rlp.Bytes(dest.Ip),
-			rlp.Uint16(dest.UdpPort),
-			rlp.Uint16(dest.TcpPort),
+		rlp.EncodeList(
+			dest.Ip,
+			bint.Encode(nil, uint64(dest.UdpPort)),
+			bint.Encode(nil, uint64(dest.TcpPort)),
 		),
-		rlp.Time(time.Now().Add(time.Hour)),
+		bint.Encode(nil, uint64(time.Now().Add(time.Hour).Unix())),
 	))
 	if err != nil {
 		return err

commit b7aea313eb18bb8fedb5006bf2ce840a843a5f41

rlp: bugfix. nested list in middle of list
Bug introduced in commit: 647880db3bcf3f4c05231a1fb17d27579234533b

When I overhauled the API for rlp decoding, I incorrectly assumed that
callers would be able to handle trailing data. However, there are cases
where we compute a hash over a decoded RLP value and so trailing data is
unacceptable. This commit now properly clamps the data returned from
Bytes() based on the length of the RLP header.

rlp/rlp.go
@@ -130,10 +130,10 @@ func (it *Iterator) Bytes() []byte {
 	case b <= list55H:
 		m, n := 1, int(b-list55L)
 		it.i += m + n
-		return data
+		return data[:m+n]
 	default:
 		m, n := decodeLength(list55H, data)
 		it.i += m + n
-		return data
+		return data[:m+n]
 	}
 }

rlp/rlp_test.go
@@ -1,6 +1,7 @@
 package rlp
 
 import (
+	"crypto/rand"
 	"encoding/hex"
 	"testing"
 
@@ -138,6 +139,33 @@ func TestDecode_List(t *testing.T) {
 			res = append(res, itr.Bytes())
 		}
 		diff.Test(t, t.Errorf, tc.input, res)
+	}
+}
+
+func rb(n int) []byte {
+	b := make([]byte, n)
+	rand.Read(b)
+	return b
+}
+
+func TestDecode_List_Nested_Long(t *testing.T) {
+	a, b, c, d, e := rb(1<<6), rb(1<<6), rb(1<<6), rb(1<<6), rb(1<<6)
+	input := List(
+		Encode(a),
+		List(Encode(b, c, d)),
+		Encode(e),
+	)
+	for i, it := 0, Iter(input); it.HasNext(); i++ {
+		switch i {
+		case 0:
+			diff.Test(t, t.Errorf, a, it.Bytes())
+		case 1:
+			diff.Test(t, t.Errorf, List(Encode(b, c, d)), it.Bytes())
+		case 2:
+			diff.Test(t, t.Errorf, e, it.Bytes())
+		default:
+			t.Fatal("should only be three items")
+		}
 	}
 }
 