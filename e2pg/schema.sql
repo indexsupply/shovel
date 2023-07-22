nalinbhardwaj/x

Add ERC4337 UserOperationEvent integration

e2pg/schema.sql
@@ -60,6 +60,28 @@ CREATE TABLE public.nft_transfers (
 );
 
 
+CREATE TABLE public.erc4337_userops (
+    contract bytea,
+    userOpHash bytea,
+    userOpSender bytea,
+    userOpPaymaster bytea,
+    userOpNonce numeric,
+    userOpSuccess boolean,
+    userOpActualGasCost numeric,
+    userOpActualGasUsed numeric,
+
+    tx_sender bytea,
+    eth numeric,
+    task_id numeric,
+    chain_id numeric,
+    block_hash bytea,
+    block_number numeric,
+    transaction_hash bytea,
+    transaction_index numeric,
+    log_index numeric
+);
+
+
 
 CREATE TABLE public.task (
     id smallint NOT NULL,

indexsupply/shovel

e2pg/schema.sql
@@ -42,6 +42,28 @@ CREATE TABLE public.erc20_transfers (
 
 
 
+CREATE TABLE public.erc4337_userops (
+    contract bytea,
+    op_hash bytea,
+    op_sender bytea,
+    op_paymaster bytea,
+    op_nonce numeric,
+    op_success boolean,
+    op_actual_gas_cost numeric,
+    op_actual_gas_used numeric,
+    tx_sender bytea,
+    eth numeric,
+    task_id numeric,
+    chain_id numeric,
+    block_hash bytea,
+    block_number numeric,
+    transaction_hash bytea,
+    transaction_index numeric,
+    log_index numeric
+);
+
+
+
 CREATE TABLE public.nft_transfers (
     contract bytea,
     token_id numeric,