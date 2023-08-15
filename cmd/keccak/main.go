7b394b59445e18ec8931efd1ac8fa4c2b8cd05ab
Ryan Smith <r@32k.io>
2023-08-15 09:47:01 -0700

    cmd/{rlpq,keccak}: new commands for debugging rlps

---
+++ cmd/keccak/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/indexsupply/x/isxhash"
+)
+
+func main() {
+	switch len(os.Args) {
+	case 1:
+		input, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		b, err := hex.DecodeString(string(input[:len(input)-1]))
+		if err != nil {
+			fmt.Println("unable to hex decode stdin")
+			os.Exit(1)
+		}
+		fmt.Printf("%x\n", isxhash.Keccak(b))
+	case 2:
+		b, err := hex.DecodeString(os.Args[1])
+		if err != nil {
+			fmt.Println("unable to hex decode argument")
+			os.Exit(1)
+		}
+		fmt.Printf("%x\n", isxhash.Keccak(b))
+	default:
+		fmt.Printf("keccak reads from stdin or through first argument")
+	}
+}
