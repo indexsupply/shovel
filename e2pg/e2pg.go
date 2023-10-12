10fd8792b03a39c90f6748c1efa1c48ea363f3c3
Ryan Smith <r@32k.io>
2023-10-12 09:46:51 -0700

    e2pg: refactor. move geth bits into separate file
    This code needs to be moved into the geth package. But that's a larger
    refactor and I don't want to do it now.

--- e2pg/e2pg.go
+++ e2pg/e2pg.go
@@ -10,13 +10,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/indexsupply/x/bint"
 	"github.com/indexsupply/x/bloom"
 	"github.com/indexsupply/x/eth"
-	"github.com/indexsupply/x/freezer"
 	"github.com/indexsupply/x/geth"
-	"github.com/indexsupply/x/jrpc"
-	"github.com/indexsupply/x/rlp"
 	"github.com/indexsupply/x/wpg"
 
 	"github.com/bmizerany/perks/quantile"
@@ -461,70 +457,5 @@ func (task *Task) writeIndex(localHash []byte, pg wpg.Conn, delta uint64) error 
 	}
 	task.stat.inum = last.Num()
 	task.stat.ihash = last.Hash()
-	return nil
-}
-
-func NewGeth(fc freezer.FileCache, rc *jrpc.Client) *Geth {
-	return &Geth{fc: fc, rc: rc}
-}
-
-type Geth struct {
-	fc freezer.FileCache
-	rc *jrpc.Client
-}
-
-func (g *Geth) ChainID() uint64 { return 0 }
-
-func (g *Geth) Hash(num uint64) ([]byte, error) {
-	return geth.Hash(num, g.fc, g.rc)
-}
-
-func (g *Geth) Latest() (uint64, []byte, error) {
-	n, h, err := geth.Latest(g.rc)
-	if err != nil {
-		return 0, nil, fmt.Errorf("getting last block hash: %w", err)
-	}
-	return bint.Uint64(n), h, nil
-}
-
-func skip(filter [][]byte, bf bloom.Filter) bool {
-	if len(filter) == 0 {
-		return false
-	}
-	for i := range filter {
-		if !bf.Missing(filter[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-func (g *Geth) LoadBlocks(filter [][]byte, bfs []geth.Buffer, blks []eth.Block) error {
-	err := geth.Load(filter, bfs, g.fc, g.rc)
-	if err != nil {
-		return fmt.Errorf("loading data: %w", err)
-	}
-	for i := range blks {
-		blks[i].Header.UnmarshalRLP(bfs[i].Header())
-		if skip(filter, bloom.Filter(blks[i].Header.LogsBloom)) {
-			continue
-		}
-		//rlp contains: [transactions,uncles]
-		blks[i].Txs.UnmarshalRLP(rlp.Bytes(bfs[i].Bodies()))
-		blks[i].Receipts.UnmarshalRLP(bfs[i].Receipts())
-	}
-	return validate(blks)
-}
-
-func validate(blocks []eth.Block) error {
-	if len(blocks) <= 1 {
-		return nil
-	}
-	for i := 1; i < len(blocks); i++ {
-		prev, curr := blocks[i-1], blocks[i]
-		if !bytes.Equal(curr.Header.Parent, prev.Hash()) {
-			return fmt.Errorf("invalid batch. prev=%s curr=%s", prev, curr)
-		}
-	}
 	return nil
 }
