e2857deb976ee58de4b25793d117a436060916be
Ryan Smith <r@32k.io>
2023-10-26 07:40:03 -0700

    e2pg: refactor. move intg prune test to the primary test file

--- e2pg/e2pg_test.go
+++ e2pg/e2pg_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"sort"
+	"strings"
 	"sync"
 	"testing"
 
@@ -303,6 +304,15 @@ func TestConverge_LocalAhead(t *testing.T) {
 	diff.Test(t, t.Errorf, task.Converge(true), ErrAhead)
 }
 
+func checkQuery(tb testing.TB, pg wpg.Conn, query string) {
+	var found bool
+	err := pg.QueryRow(context.Background(), query).Scan(&found)
+	diff.Test(tb, tb.Fatalf, err, nil)
+	if !found {
+		tb.Errorf("query\n%s\nreturned false", query)
+	}
+}
+
 func TestPruneTask(t *testing.T) {
 	pg := testpg(t)
 	it := func(n uint8) {
@@ -320,4 +330,48 @@ func TestPruneTask(t *testing.T) {
 	checkQuery(t, pg, `select count(*) = 10 from e2pg.task`)
 	PruneTask(context.Background(), pg, 1)
 	checkQuery(t, pg, `select count(*) = 1 from e2pg.task`)
+}
+
+func TestPruneIntg(t *testing.T) {
+	ctx := context.Background()
+
+	pqxtest.CreateDB(t, Schema)
+	pg, err := pgxpool.New(ctx, pqxtest.DSNForTest(t))
+	diff.Test(t, t.Fatalf, err, nil)
+
+	iub := newIUB(1)
+	iub.updates[0].Name = "foo"
+	iub.updates[0].SrcName = "bar"
+	iub.updates[0].Num = 1
+
+	err = iub.write(ctx, pg)
+	diff.Test(t, t.Fatalf, err, nil)
+	checkQuery(t, pg, `select count(*) = 1 from e2pg.intg`)
+
+	err = iub.write(ctx, pg)
+	if err == nil || !strings.Contains(err.Error(), "intg_name_src_name_num_idx1") {
+		t.Errorf("expected 2nd write to return unique error")
+	}
+
+	for i := 0; i < 10; i++ {
+		iub.updates[0].Num = uint64(i + 2)
+		err := iub.write(ctx, pg)
+		diff.Test(t, t.Fatalf, err, nil)
+	}
+	checkQuery(t, pg, `select count(*) = 11 from e2pg.intg`)
+	err = PruneIntg(ctx, pg, 10)
+	diff.Test(t, t.Fatalf, err, nil)
+	checkQuery(t, pg, `select count(*) = 10 from e2pg.intg`)
+
+	iub.updates[0].Name = "foo"
+	iub.updates[0].SrcName = "baz"
+	iub.updates[0].Num = 1
+	err = iub.write(ctx, pg)
+	diff.Test(t, t.Fatalf, err, nil)
+	checkQuery(t, pg, `select count(*) = 1 from e2pg.intg where src_name = 'baz'`)
+	checkQuery(t, pg, `select count(*) = 11 from e2pg.intg`)
+
+	err = PruneIntg(ctx, pg, 10)
+	diff.Test(t, t.Fatalf, err, nil)
+	checkQuery(t, pg, `select count(*) = 11 from e2pg.intg`)
 }
