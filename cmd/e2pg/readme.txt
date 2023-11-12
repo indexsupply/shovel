c53f751726102c761bbf266b29ec638384bae70a
Ryan Smith <r@32k.io>
2023-11-11 18:49:29 -0800

    shovel: s/e2pg/shovel (#202)

--- cmd/e2pg/readme.txt
+++
@@ -1,3 +0,0 @@
-Building a docker image
-	In the root directory:
-	docker build -t e2pg:latest -f ./cmd/e2pg/Dockerfile .
--- shovel/schema.sql
+++ shovel/schema.sql
@@ -12,7 +12,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
-CREATE SCHEMA e2pg;
+CREATE SCHEMA shovel;
 
 
 SET default_tablespace = '';
@@ -20,7 +20,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 
-CREATE TABLE e2pg.ig_updates (
+CREATE TABLE shovel.ig_updates (
     name text NOT NULL,
     src_name text NOT NULL,
     backfill boolean DEFAULT false,
@@ -32,14 +32,14 @@ CREATE TABLE e2pg.ig_updates (
 
 
 
-CREATE TABLE e2pg.integrations (
+CREATE TABLE shovel.integrations (
     name text,
     conf jsonb
 );
 
 
 
-CREATE TABLE e2pg.migrations (
+CREATE TABLE shovel.migrations (
     idx integer NOT NULL,
     hash bytea NOT NULL,
     inserted_at timestamp with time zone DEFAULT now() NOT NULL
@@ -47,7 +47,7 @@ CREATE TABLE e2pg.migrations (
 
 
 
-CREATE TABLE e2pg.sources (
+CREATE TABLE shovel.sources (
     name text,
     chain_id integer,
     url text
@@ -55,7 +55,7 @@ CREATE TABLE e2pg.sources (
 
 
 
-CREATE TABLE e2pg.task_updates (
+CREATE TABLE shovel.task_updates (
     num bigint,
     hash bytea,
     insert_at timestamp with time zone DEFAULT now(),
@@ -71,28 +71,28 @@ CREATE TABLE e2pg.task_updates (
 
 
 
-ALTER TABLE ONLY e2pg.migrations
+ALTER TABLE ONLY shovel.migrations
     ADD CONSTRAINT migrations_pkey PRIMARY KEY (idx, hash);
 
 
 
-CREATE UNIQUE INDEX intg_name_src_name_backfill_num_idx ON e2pg.ig_updates USING btree (name, src_name, backfill, num DESC);
+CREATE UNIQUE INDEX intg_name_src_name_backfill_num_idx ON shovel.ig_updates USING btree (name, src_name, backfill, num DESC);
 
 
 
-CREATE UNIQUE INDEX sources_name_chain_id_idx ON e2pg.sources USING btree (name, chain_id);
+CREATE UNIQUE INDEX sources_name_chain_id_idx ON shovel.sources USING btree (name, chain_id);
 
 
 
-CREATE UNIQUE INDEX sources_name_idx ON e2pg.sources USING btree (name);
+CREATE UNIQUE INDEX sources_name_idx ON shovel.sources USING btree (name);
 
 
 
-CREATE UNIQUE INDEX task_src_name_num_idx ON e2pg.task_updates USING btree (src_name, num DESC) WHERE (backfill = true);
+CREATE UNIQUE INDEX task_src_name_num_idx ON shovel.task_updates USING btree (src_name, num DESC) WHERE (backfill = true);
 
 
 
-CREATE UNIQUE INDEX task_src_name_num_idx1 ON e2pg.task_updates USING btree (src_name, num DESC) WHERE (backfill = false);
+CREATE UNIQUE INDEX task_src_name_num_idx1 ON shovel.task_updates USING btree (src_name, num DESC) WHERE (backfill = false);
 
 
 
