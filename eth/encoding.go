094c909b39a7166e547851246aff1694739d8253
Ryan Smith <r@32k.io>
2023-12-28 16:36:42 -0800

    eth: add encoding/decoding for uint64

--- eth/encoding.go
+++ eth/encoding.go
@@ -1,6 +1,9 @@
 package eth
 
-import "encoding/hex"
+import (
+	"encoding/hex"
+	"strconv"
+)
 
 // deals with eth's 0x prefix and possible odd length
 func DecodeHex(s string) []byte {
@@ -12,4 +15,29 @@ func DecodeHex(s string) []byte {
 	}
 	h, _ := hex.DecodeString(s)
 	return h
+}
+
+// 0x prefixed hex encoded string
+func EncodeHex(b []byte) string {
+	return "0x" + hex.EncodeToString(b)
+}
+
+// deals with eth's 0x prefix and possible odd length
+func DecodeUint64(s string) uint64 {
+	if len(s) >= 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X') {
+		s = s[2:]
+	}
+	if len(s)%2 == 1 {
+		s = "0" + s
+	}
+	n, err := strconv.ParseUint(s, 16, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+// 0x prefixed hex encoded uint64
+func EncodeUint64(n uint64) string {
+	return "0x" + strconv.FormatUint(n, 16)
 }
--- eth/encoding_test.go
+++ eth/encoding_test.go
@@ -30,5 +30,29 @@ func TestDecodeHex(t *testing.T) {
 	}
 	for _, tc := range cases {
 		diff.Test(t, t.Errorf, DecodeHex(tc.input), tc.want)
+		diff.Test(t, t.Errorf, DecodeHex(EncodeHex(tc.want)), tc.want)
+	}
+}
+
+func TestDecodeUint64(t *testing.T) {
+	cases := []struct {
+		input string
+		want  uint64
+	}{
+		{
+			input: "0x2a",
+			want:  42,
+		},
+		{
+			input: "2a",
+			want:  42,
+		},
+		{
+			input: "2",
+			want:  2,
+		},
+	}
+	for _, tc := range cases {
+		diff.Test(t, t.Errorf, DecodeUint64(tc.input), tc.want)
 	}
 }
