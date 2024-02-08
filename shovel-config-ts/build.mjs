cb932e71ae916a9e6c0972f78552e929119f4c0a
Ryan Smith <r@32k.io>
2024-02-07 19:27:24 -0800

    wip

--- shovel-config-ts/build.mjs
+++ shovel-config-ts/build.mjs
@@ -1,8 +1,6 @@
-import dts from "bun-plugin-dts";
-
 await Bun.build({
   entrypoints: ["./src/index.ts"],
   outdir: "./dist",
-  minify: true,
-  plugins: [dts()],
+  target: "node",
+  format: "esm",
 });
