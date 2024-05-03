6a8356b0ccb3b6445e55f3ebb859d44277bcda4a
ryan smith <r@32k.io>
2024-05-03 09:13:57 -0700

    tc: fail test on error. want err helper

--- tc/testcheck.go
+++ tc/testcheck.go
@@ -10,11 +10,19 @@ import (
 func NoErr(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
-		t.Errorf("expected no error. got: %s", err)
+		t.Fatalf("expected no error. got: %s", err)
+	}
+}
+
+func WantErr(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Fatalf("expected error. got: %s", err)
 	}
 }
 
 func WantGot(tb testing.TB, want, got any) {
+	tb.Helper()
 	if !reflect.DeepEqual(want, got) {
 		tb.Error(pretty.Sprintf("want: %v got: %v", want, got))
 	}
