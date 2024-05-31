indexsupply/shovel

commit 42721b26a79509c3847f5f9ad1b8e5adbaf01391

shovel/div: add new filter operations

dig/dig.go
@@ -386,51 +386,91 @@ type Filter struct {
 }
 
 func (f Filter) Accept(ctx context.Context, pgmut *sync.Mutex, pg wpg.Conn, d any) (bool, error) {
-	var val []byte
+	if len(f.Arg) == 0 {
+		return true, nil
+	}
+	switch v := d.(type) {
+	case eth.Bytes:
+		d = []byte(v)
+	case eth.Uint64:
+		d = uint64(v)
+	}
 	switch v := d.(type) {
 	case []byte:
-		val = []byte(v)
-	case eth.Bytes:
-		val = []byte(v)
-	default:
-		return true, nil
-	}
-
-	switch {
-	case strings.HasSuffix(f.Op, "contains"):
-		var res bool
 		switch {
-		case len(f.Ref.Table) > 0:
-			q := fmt.Sprintf(
-				`select true from %s where %s = $1`,
-				f.Ref.Table,
-				f.Ref.Column,
-			)
-			pgmut.Lock()
-			defer pgmut.Unlock()
-			err := pg.QueryRow(ctx, q, val).Scan(&res)
+		case strings.HasSuffix(f.Op, "contains"):
+			var res bool
 			switch {
-			case errors.Is(err, pgx.ErrNoRows):
-				res = false
-			case err != nil:
-				const tag = "filter using reference (%s %s): %w"
-				return false, fmt.Errorf(tag, f.Ref.Table, f.Ref.Column, err)
-			}
-		default:
-			for i := range f.Arg {
-				if bytes.Contains(val, eth.DecodeHex(f.Arg[i])) {
-					res = true
-					break
+			case len(f.Ref.Table) > 0:
+				q := fmt.Sprintf(
+					`select true from %s where %s = $1`,
+					f.Ref.Table,
+					f.Ref.Column,
+				)
+				pgmut.Lock()
+				defer pgmut.Unlock()
+				err := pg.QueryRow(ctx, q, v).Scan(&res)
+				switch {
+				case errors.Is(err, pgx.ErrNoRows):
+					res = false
+				case err != nil:
+					const tag = "filter using reference (%s %s): %w"
+					return false, fmt.Errorf(tag, f.Ref.Table, f.Ref.Column, err)
+				}
+			default:
+				for i := range f.Arg {
+					if bytes.Contains(v, eth.DecodeHex(f.Arg[i])) {
+						res = true
+						break
+					}
 				}
 			}
+			if strings.HasPrefix(f.Op, "!") {
+				return !res, nil
+			}
+			return res, nil
+		default:
+			return true, nil
 		}
-		if strings.HasPrefix(f.Op, "!") {
-			return !res, nil
+	case string:
+		switch f.Op {
+		case "eq":
+			return v == f.Arg[0], nil
+		case "ne":
+			return v != f.Arg[0], nil
 		}
-		return res, nil
-	default:
-		return true, nil
+	case uint64:
+		i, err := strconv.ParseUint(f.Arg[0], 10, 64)
+		if err != nil {
+			return false, fmt.Errorf("unable to convert filter arg to int: %q", f.Arg[0])
+		}
+		switch f.Op {
+		case "eq":
+			return v == i, nil
+		case "ne":
+			return v != i, nil
+		case "gt":
+			return v > i, nil
+		case "lt":
+			return v < i, nil
+		}
+	case *uint256.Int:
+		i := &uint256.Int{}
+		if err := i.SetFromDecimal(f.Arg[0]); err != nil {
+			return false, fmt.Errorf("unable to convert filter arg dec to uint256: %q", f.Arg[0])
+		}
+		switch f.Op {
+		case "eq":
+			return v.Cmp(i) == 0, nil
+		case "ne":
+			return v.Cmp(i) != 0, nil
+		case "gt":
+			return v.Cmp(i) == 1, nil
+		case "lt":
+			return v.Cmp(i) == -1, nil
+		}
 	}
+	return true, nil
 }
 
 func parseArray(elm atype, s string) atype {

dig/dig_test.go
@@ -1,15 +1,36 @@
 package dig
 
 import (
+	"context"
+	"database/sql"
 	"encoding/hex"
 	"reflect"
 	"strings"
+	"sync"
 	"testing"
 
+	"blake.io/pqx/pqxtest"
+	"github.com/holiman/uint256"
 	"github.com/indexsupply/x/bint"
+	"github.com/indexsupply/x/eth"
+	"github.com/indexsupply/x/tc"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 
 	"kr.dev/diff"
 )
+
+func TestMain(m *testing.M) {
+	sql.Register("postgres", stdlib.GetDefaultDriver())
+	pqxtest.TestMain(m)
+}
+
+func testpg(t *testing.T) *pgxpool.Pool {
+	pqxtest.CreateDB(t, "")
+	pg, err := pgxpool.New(context.Background(), pqxtest.DSNForTest(t))
+	tc.NoErr(t, err)
+	return pg
+}
 
 func TestHasStatic(t *testing.T) {
 	cases := []struct {
@@ -367,4 +388,49 @@ func TestNumIndexed(t *testing.T) {
 		},
 	}
 	diff.Test(t, t.Errorf, 3, event.numIndexed())
+}
+
+func TestFilter(t *testing.T) {
+	dec2uint256 := func(s string) *uint256.Int {
+		i, _ := uint256.FromDecimal(s)
+		return i
+	}
+	pg := testpg(t)
+	mt := new(sync.Mutex)
+	cases := []struct {
+		f    Filter
+		d    any
+		want bool
+	}{
+		{
+			Filter{Op: "gt", Arg: []string{"1"}},
+			eth.Uint64(0),
+			false,
+		},
+		{
+			Filter{Op: "gt", Arg: []string{"1"}},
+			eth.Uint64(2),
+			true,
+		},
+		{
+			Filter{Op: "eq", Arg: []string{"340282366920938463463374607431768211456"}},
+			dec2uint256("340282366920938463463374607431768211456"),
+			true,
+		},
+		{
+			Filter{Op: "eq", Arg: []string{"foo"}},
+			"foo",
+			true,
+		},
+		{
+			Filter{Op: "ne", Arg: []string{"bar"}},
+			"foo",
+			true,
+		},
+	}
+	for _, c := range cases {
+		got, err := c.f.Accept(context.Background(), mt, pg, c.d)
+		tc.NoErr(t, err)
+		tc.WantGot(t, c.want, got)
+	}
 }

commit 0f7417e0bbccfadf4aea5dbce311aeda0ad2fbc0

shovel: bugfix. filter ref was not filtering
This was a regression from: 42721b26a79509c3847f5f9ad1b8e5adbaf01391

When the accept method was re-written, little care was taken to not
break filter references. The fix was simple, only exit early if there
are no args AND there are not filter references.

dig/dig.go
@@ -387,7 +387,7 @@ type Filter struct {
 }
 
 func (f Filter) Accept(ctx context.Context, pgmut *sync.Mutex, pg wpg.Conn, d any) (bool, error) {
-	if len(f.Arg) == 0 {
+	if len(f.Arg) == 0 && len(f.Ref.Integration) == 0 {
 		return true, nil
 	}
 	switch v := d.(type) {

tc/testcheck.go
@@ -7,10 +7,10 @@ import (
 	"github.com/kr/pretty"
 )
 
-func NoErr(t *testing.T, err error) {
-	t.Helper()
+func NoErr(tb testing.TB, err error) {
+	tb.Helper()
 	if err != nil {
-		t.Fatalf("expected no error. got: %s", err)
+		tb.Fatalf("expected no error. got: %s", err)
 	}
 }
 

shovel/integration_test.go
@@ -8,45 +8,26 @@ import (
 
 	"github.com/indexsupply/x/jrpc2"
 	"github.com/indexsupply/x/shovel/config"
+	"github.com/indexsupply/x/tc"
 	"github.com/indexsupply/x/wpg"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"kr.dev/diff"
 )
-
-func check(t testing.TB, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-// Process will download the header,bodies, and receipts data
-// if it doesn't exist in: integrations/testdata
-// In the case that it needs to fetch the data, an RPC
-// client will be used. The RPC endpoint needs to support
-// the debug_dbAncient and debug_dbGet methods.
-func process(tb testing.TB, pg *pgxpool.Pool, conf config.Root, n uint64) *Task {
-	check(tb, config.ValidateFix(&conf))
-	check(tb, config.Migrate(context.Background(), pg, conf))
-
-	task, err := NewTask(
-		WithPG(pg),
-		WithSource(jrpc2.New("https://ethereum.publicnode.com")),
-		WithIntegration(conf.Integrations[0]),
-		WithRange(n, n+1),
-	)
-	check(tb, err)
-	check(tb, task.Converge())
-	return task
-}
 
 func TestIntegrations(t *testing.T) {
 	cases := []struct {
-		blockNum    uint64
-		config      string
-		queries     []string
-		deleteQuery string
+		blockNum uint64
+		config   string
+		check    []string
+		delete   string
 	}{
+		{
+			17943843,
+			"filter-ref.json",
+			[]string{
+				`select count(*) = 1 from tx`,
+				`select count(*) = 1 from tx_w_block`,
+			},
+			"select count(*) = 0 from tx",
+		},
 		{
 			19583743,
 			"receipt.json",
@@ -124,30 +105,45 @@ func TestIntegrations(t *testing.T) {
 			"select count(*) = 0 from seaport_test",
 		},
 	}
-	for _, tc := range cases {
-		pg := wpg.TestPG(t, Schema)
-		conf := config.Root{Integrations: []config.Integration{{}}}
-		decode(t, read(t, tc.config), &conf.Integrations[0])
-		task := process(t, pg, conf, tc.blockNum)
-		for i, q := range tc.queries {
-			var found bool
-			err := pg.QueryRow(context.Background(), q).Scan(&found)
-			diff.Test(t, t.Errorf, nil, err)
-			if err != nil {
-				t.Logf("failing test query: %d", i)
-			}
-			if !found {
-				t.Errorf("test %s failed", tc.config)
-			}
+	for _, c := range cases {
+		var (
+			ctx  = context.Background()
+			pg   = wpg.TestPG(t, Schema)
+			conf = config.Root{Integrations: []config.Integration{{}}}
+		)
+		decode(t, read(t, c.config), &conf.Integrations)
+		tc.NoErr(t, config.ValidateFix(&conf))
+		tc.NoErr(t, config.Migrate(ctx, pg, conf))
+		for _, ig := range conf.Integrations {
+			task, err := NewTask(
+				WithPG(pg),
+				WithSource(jrpc2.New("https://ethereum.publicnode.com")),
+				WithIntegration(ig),
+				WithRange(c.blockNum, c.blockNum+1),
+			)
+			tc.NoErr(t, err)
+			tc.NoErr(t, task.Converge())
 		}
+		check(t, c.config, pg, c.check...)
+		for _, ig := range conf.Integrations {
+			task, err := NewTask(
+				WithPG(pg),
+				WithSource(jrpc2.New("https://ethereum.publicnode.com")),
+				WithIntegration(ig),
+			)
+			tc.NoErr(t, err)
+			tc.NoErr(t, task.Delete(pg, c.blockNum))
+			check(t, c.config, pg, c.delete)
+		}
+	}
+}
 
-		check(t, task.Delete(pg, tc.blockNum))
-
-		var deleted bool
-		err := pg.QueryRow(context.Background(), tc.deleteQuery).Scan(&deleted)
-		diff.Test(t, t.Errorf, nil, err)
-		if !deleted {
-			t.Errorf("%s was not cleaned up after ig.Delete", tc.config)
+func check(t *testing.T, name string, pg wpg.Conn, queries ...string) {
+	for i, q := range queries {
+		var res bool
+		tc.NoErr(t, pg.QueryRow(context.Background(), q).Scan(&res))
+		if !res {
+			t.Errorf("query %s/%d failed: %q", name, i, q)
 		}
 	}
 }